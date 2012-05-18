bd9bc2c5b37e6855e5763a38df62460b85f4f909
Daniel Gempesaw <dgempesaw@sharecare.com>
2012-05-18 11:47:54 -0400

    adding a readme. adding a defun.el file that includes a google search.

---
+++ defun.el
@@ -0,0 +1,9 @@
+(defun prelude-google ()
+  "Googles a query or region if any."
+  (interactive)
+  (browse-url
+   (concat
+    "http://www.google.com/search?ie=utf-8&oe=utf-8&q="
+    (url-hexify-string (if mark-active
+         (buffer-substring (region-beginning) (region-end))
+       (read-string "Google: "))))))
