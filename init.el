ce3a0a6b009c41f9f8bcbab0d7355c75d790ecbb
Daniel Gempesaw <gempesaw@gmail.com>
2012-05-19 20:37:07 -0400

    oh, so _that's_ how you use el-get. I feel pretty foolish using it incorrectly up until now, but hooray :) now it is working well.

--- init.el
+++ init.el
@@ -1,5 +1,29 @@
 ;; set up el-get
-(add-to-list 'load-path "~/.emacs.d/el-get/el-get") (unless (require 'el-get nil t) (with-current-buffer (url-retrieve-synchronously "https://raw.github.com/dimitri/el-get/master/el-get-install.el") (end-of-buffer) (eval-print-last-sexp))) (el-get 'sync)
+(add-to-list 'load-path "~/.emacs.d/el-get/el-get")
+(unless (require 'el-get nil t)
+  (with-current-buffer
+      (url-retrieve-synchronously "https://raw.github.com/dimitri/el-get/master/el-get-install.el")
+    (end-of-buffer)
+    (eval-print-last-sexp)))
+
+(setq el-get-sources
+      '((:name tumblr-mode
+             :type git
+             :url "http://github.com/qxj/tumblr-mode.git"
+             :features (tumblr-mode))))
+
+(setq my-packages
+      (append
+       '(el-get
+         cperl-mode
+         crontab-mode
+         cssh
+         ;; switch-window
+         magit
+         php-mode-improved)
+       (mapcar 'el-get-source-name el-get-sources)))
+
+(el-get 'sync my-packages)
 
 ;; load the child files
 (add-to-list 'load-path "~/.emacs.d/")
@@ -9,16 +33,3 @@
 (load "modes.el")
 (load "tabs.el")
 (load "themes.el")
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-safe-themes (quote ("d2622a2a2966905a5237b54f35996ca6fda2f79a9253d44793cfe31079e3c92b" "501caa208affa1145ccbb4b74b6cd66c3091e41c5bb66c677feda9def5eab19c" "6fb8d6a7208505c2e51466280e6abc4786e2f97c04698e605de857a1f533cd0a" "fc5fcb6f1f1c1bc01305694c59a1a861b008c534cae8d0e48e4d5e81ad718bc6" default))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
