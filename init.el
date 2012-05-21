13e1f7e4c7b9122e8fb62068690eece692ca8a61
Daniel Gempesaw <dgempesaw@sharecare.com>
2012-05-21 15:05:58 -0400

    updating init.el with some commented out packages that I was having trouble installing. turning yas/hippie expand off because i don't have yas installed. yet.

--- init.el
+++ init.el
@@ -16,11 +16,12 @@
       (append
        '(;; el-get
          cperl-mode
-         crontab-mode
+         ;; crontab-mode
          cssh
          ;; switch-window
          magit
-         php-mode-improved)
+         ;;php-mode-improved
+         )
        (mapcar 'el-get-source-name el-get-sources)))
 
 (el-get 'sync my-packages)
@@ -33,3 +34,15 @@
 (load "modes.el")
 (load "tabs.el")
 (load "themes.el")
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes (quote ("6fb8d6a7208505c2e51466280e6abc4786e2f97c04698e605de857a1f533cd0a" default))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
--- tabs.el
+++ tabs.el
@@ -25,7 +25,7 @@
 ;; http://www.emacswiki.org/emacs/TabCompletion
 ;; TODO: Take a look at https://github.com/genehack/smart-tab
 
-(defvar smart-tab-using-hippie-expand t
+(defvar smart-tab-using-hippie-expand -1
   "turn this on if you want to use hippie-expand completion.")
 
 (defun smart-tab (prefix)
