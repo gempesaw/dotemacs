89416d6917f75306c4edf206b60053d4fd6700ed
Daniel Gempesaw <dgempesaw@sharecare.com>
2012-07-09 16:55:53 -0400

    Add: redefine M-x term not to ask for program command; breaks windows
    I'm tired of M-x term always asking me for the program command to run,
    as it's almost always /bin/bash, so I've gone in and redefined term so
    that it won't ask. However, this makes it much less likely to work on
    a Windows box, as they won't have bash there. I'll fix that if and
    when I need to. For now, this works on OS X and probably linux.

--- defun.el
+++ defun.el
@@ -7,3 +7,20 @@
     (url-hexify-string (if mark-active
          (buffer-substring (region-beginning) (region-end))
        (read-string "Google: "))))))
+
+;; I don't want M-x term to ask me about what shell to run
+;; this is pulled from term.el (C-h f term)
+(defun term ()
+  "Start a terminal-emulator in a new buffer.
+The buffer is in Term mode; see `term-mode' for the commands to
+use in that buffer.
+
+I've redefined this in `defun.el', so that it doesn't ask for the
+shell to run.
+
+\\<term-raw-map>Type \\[switch-to-buffer] to switch to another buffer."
+  (interactive)
+  (set-buffer (make-term "terminal" "/bin/bash"))
+  (term-mode)
+  (term-char-mode)
+  (switch-to-buffer "*terminal*"))
