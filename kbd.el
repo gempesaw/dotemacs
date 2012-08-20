58bf2dc2988501d8f82830b9e849694d690f51b4
Daniel Gempesaw <dgempesaw@sharecare.com>
2012-08-20 16:25:18 -0400

    Add keybindings for mark-multiple

--- kbd.el
+++ kbd.el
@@ -90,3 +90,12 @@
   '(progn
      (global-set-key "\C-xo" 'win-switch-dispatch)
      ))
+;; for multiple marking
+(eval-after-load "mark-more-like-this"
+  '(progn
+     (global-set-key (kbd "C-<") 'mark-previous-like-this)
+     (global-set-key (kbd "C->") 'mark-next-like-this)
+     (global-set-key (kbd "C-M-m") 'mark-more-like-this) ; like the other two, but takes an argument (negative is previous)
+     (global-set-key (kbd "C-*") 'mark-all-like-this)
+     ))
+
