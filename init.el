a7d6f0cb868e24253fbd7f464d508fe1d50d4eda
Daniel Gempesaw <dgempesaw@sharecare.com>
2012-09-05 16:00:57 -0400

    Add chromedriver binary path for hdew tests.

--- init.el
+++ init.el
@@ -82,7 +82,7 @@
 ;; start up selenium if possible.
 (if (eq nil (get-buffer "selenium"))
     (when (file-exists-p "/opt/selenium-server-standalone-2.25.0.jar")
-      (shell-command "java -jar /opt/selenium-server-standalone-2.25.0.jar &")
+      (shell-command "java -jar /opt/selenium-server-standalone-2.25.0.jar -Dwebdriver.chrome.driver=/opt/chromedriver &")
       (set-buffer "*Async Shell Command*")
       (rename-buffer "selenium")
       (toggle-read-only)))
