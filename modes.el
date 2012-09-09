3888af120cb52e48f6996c2423bd0100da883ba9
Daniel Gempesaw <dgempesaw@sharecare.com>
2012-09-08 20:57:10 -0400

    Update snippets folders. submodule update "snippets"

--- modes.el
+++ modes.el
@@ -74,8 +74,8 @@
 ;; personal snippets
 (setq yas-snippet-dirs
       '("~/.emacs.d/snippets"            ;; personal snippets
-	"~/.emacs.d/el-get/yasnippet/snippets/"    ;; the default collection
-	))
+        "~/.emacs.d/el-get/yasnippet/snippets/"    ;; the default collection
+        ))
 (yas/global-mode 1)
 
 (win-switch-mode t)
