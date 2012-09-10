1bfff4b0b63f72a2cfed68930ad7ad9391dd331e
Daniel Gempesaw <dgempesaw@sharecare.com>
2012-09-09 21:13:22 -0400

    Switch default theme to wheatgrass

--- emacs-custom.el
+++ emacs-custom.el
@@ -3,7 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-enabled-themes (quote (tango-dark)))
+ '(custom-enabled-themes (quote (wheatgrass)))
  '(custom-safe-themes (quote ("1d1e97c4804a04441b08226e4775295843baae033673e63513ae925eafcdc9bf" "d2622a2a2966905a5237b54f35996ca6fda2f79a9253d44793cfe31079e3c92b" "38784c7094a1df1cae422c86315318c33ec4c5da8d9646bebdc434ce83a70ecf" "66c133d8ee85853cbafb0fde95334057e61e9bb9dc7e3188a2389fb565efe6fd" "459e760e4d16ec11821721d9e39bf315116b2eb396bc0fd9e1e3f68a285ff82d" "183884e94bca3760f6e15e34106e3b21eda32868867e66c5db36be7ed5ed35ef" "ce1b305bc7613cd3ee298f1d8149e7565e87e19cebfaa4e6e91af46f1ad2d768" "0e14a39ac94df99f31f7110f2e806c52501e702ace9a6a6a24d3636904baa2f0" "83f0f087feac843259b3f6795b7a721995b48001f4303015c4d665a38f0c11ac" "b812e81cbf56a7b8326fec621e04eadcb4b3b723189bcaaee459c33cb47e5d3b" "3f833eb2a2da85f670c2c4efdfee4d58ac26d56016715361f57b71c943aa7701" "501caa208affa1145ccbb4b74b6cd66c3091e41c5bb66c677feda9def5eab19c" "6fb8d6a7208505c2e51466280e6abc4786e2f97c04698e605de857a1f533cd0a" default)))
  '(fci-rule-color "#eee8d5"))
 (custom-set-faces
