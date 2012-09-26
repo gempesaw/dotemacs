0f2829d007ed18a4b67749acbb5229bb3cd104d3
Daniel Gempesaw <dgempesaw@sharecare.com>
2012-09-26 18:10:08 -0400

    Move customization of custom file into customize.el. lol.

--- customize.el
+++ customize.el
@@ -136,16 +136,7 @@
   '(progn
      (require 'magit-svn)))
 
-;; for multiple marking
-;; (eval-after-load "mark-more-like-this"
-;;   '(progn
-;;      (global-set-key (kbd "C-<") 'mark-previous-like-this)
-;;      (global-set-key (kbd "C->") 'mark-next-like-this)
-;;      (global-set-key (kbd "C-M-m") 'mark-more-like-this) ; like the other two, but takes an argument (negative is previous)
-;;      (global-set-key (kbd "C-*") 'mark-all-like-this)
-;;      ))
-
-
+(setq custom-file "~/emacs-custom.el")
 
 (eval-after-load "cperl-mode"
   '(progn
