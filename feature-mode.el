311b3da98bfee46f3ee709dc63b45f948cd1e4fe
Daniel Gempesaw <gempesaw@gmail.com>
2013-02-13 09:57:21 -0500

    Enhance default feature mode

--- feature-mode.el
+++ feature-mode.el
@@ -2,12 +2,28 @@
 
 (define-generic-mode 'feature-mode
   '("# ")                     ;; comment characters
-  '("Feature" "Scenario"      ;; keywords to font-lock
-    "Hostname" "Tags"
+  '("Feature"                 ;; keywords to font-lock
+    "Scenario"
+    "Hostname"
+    "Tags"
     "Subtitle")
   nil                         ;; additional expressions to font-lock
   '("\\.feature$")            ;; placed in auto-mode-alist
-  ;; '(feature-compile-shortcut) ;; list of functions to call
+  '(feature-mode-hook) ;; list of functions to call)
   "A mode for editing automation .feature files."
-
   )
+
+(defun feature-mode-hook ()
+    "feature major mode
+
+\\{feature-mode-map}"
+  (setq major-mode 'feature-mode
+        mode-name "Feature"
+        mode-line-process "Feature")
+  (use-local-map feature-mode-map)
+  (run-mode-hooks 'feature-mode-hook))
+
+(defvar feature-mode-map
+    (let ((map (make-keymap)))
+      (define-key map (kbd "<f5>") 'execute-feature)
+      map))
