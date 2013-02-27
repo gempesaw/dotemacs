e8491ecea64e64c11b2a6190851403c49831da22
Daniel Gempesaw <gempesaw@gmail.com>
2013-02-27 10:09:03 -0500

    Have ~ replace itself by ~/ in ido-find-file

--- hooks.el
+++ hooks.el
@@ -1,1 +1,12 @@
 (add-hook 'minibuffer-setup-hook 'my-minibuffer-setup-hook)
+
+(add-hook 'ido-setup-hook
+ (lambda ()
+   ;; Go straight home
+   (define-key ido-file-completion-map
+     (kbd "~")
+     (lambda ()
+       (interactive)
+       (if (looking-back "/")
+           (insert "~/")
+         (call-interactively 'self-insert-command))))))
