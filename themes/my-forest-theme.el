2c83ee275bea84c5b23d294b14c5d9e686b0fb12
gempesaw <gempesaw@gmail.com>
2013-03-28 11:16:05 -0400

    Remove manual themes in favor of marmalade-available packages

--- themes/my-forest-theme.el
+++
@@ -1,8 +0,0 @@
-(deftheme my-forest
-  "Created 2012-06-07.")
-
-(custom-theme-set-faces
- 'my-forest
- '(default ((t (:inherit nil :stipple nil :background "gray12" :foreground "green" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 120 :width normal :foundry "apple" :family "Monaco")))))
-
-(provide-theme 'my-forest)
