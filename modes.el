86d23c5bbb12a3f24b13f72fc49c54e830a3a708
gempesaw <gempesaw@gmail.com>
2013-03-28 11:14:52 -0400

    Cleanup - most stuff in modes belogns somewhere else (customize, hooks)

--- modes.el
+++ modes.el
@@ -5,24 +5,6 @@
 (when (fboundp 'winner-mode)
   (winner-mode 1))
 
-;; Use cperl-mode instead of the default perl-mode
-(defalias 'perl-mode 'cperl-mode)
-(setq cperl-invalid-face (quote off))
-(setq cperl-electric-keywords t)
-(setq cperl-indent-level 4)
-(setq cperl-continued-statement-offset 0)
-(setq cperl-extra-newline-before-brace t)
-(setq cperl-indent-parens-as-block t)
-
-(add-hook 'cperl-mode-hook
-  (lambda()
-    (local-set-key (kbd "C-c C-s C-k") 'hs-show-block)
-    (local-set-key (kbd "C-c C-s C-j") 'hs-hide-block)
-    (local-set-key (kbd "C-c C-s C-p") 'hs-hide-all)
-    (local-set-key (kbd "C-c C-s C-n") 'hs-show-all)
-    (local-set-key (kbd "<f5>") 'execute-perl)
-    (hs-minor-mode t)))
-
 ;; ido mode
 (ido-mode t) ; enable ido for buffer/file switching
 (ido-everywhere t) ;enable ido everywhere
@@ -42,26 +24,13 @@
 ;; smart pairing for all
 ;; (electric-pair-mode t)
 
-;; dired - reuse current buffer by pressing 'a'
-(put 'dired-find-alternate-file 'disabled nil)
-
 ;; clean up obsolete buffers automatically
 ;; (require 'midnight)
 
 ;; the blinking cursor is nothing, but an annoyance
 (blink-cursor-mode -1)
 
-;; turn on html mode for .tag files
-(setq auto-mode-alist (cons '("\\.tag$" . html-mode) auto-mode-alist))
-(setq auto-mode-alist (cons '("\\.out$" . auto-revert-tail-mode) auto-mode-alist))
-
 (global-subword-mode t)
-
-;; personal snippets
-(setq yas/snippet-dirs
-      '("~/.emacs.d/snippets"            ;; personal snippets
-        "~/.emacs.d/el-get/yasnippet/snippets/"    ;; the default collection
-        ))
 
 (yas-global-mode 1)
 
@@ -71,20 +40,4 @@
 (when (memq window-system '(mac ns))
   (exec-path-from-shell-initialize))
 
-(eval-after-load "paredit"
-  '(progn
-     (put 'paredit-forward-delete 'delete-selection 'supersede)
-     (put 'paredit-backward-delete 'delete-selection 'supersede)
-     (put 'paredit-open-round 'delete-selection t)
-     (put 'paredit-open-square 'delete-selection t)
-     (put 'paredit-doublequote 'delete-selection t)
-     (put 'paredit-newline 'delete-selection t)
-     ))
-
-(delete '("\\.js\\'" . javascript-generic-mode) auto-mode-alist)
-(delete '("\\.js\\'" . js-mode) auto-mode-alist)
-(delete '("\\.t$'" . cperl-mode) auto-mode-alist)
-(add-to-list 'auto-mode-alist '("\\.js$'" . js2-mode))
-(add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
-(add-to-list 'auto-mode-alist '("\\.t$" . cperl-mode))
-(add-to-list 'auto-mode-alist '("\\.md$" . markdown-mode))
+(autoload 'js2-mode "js2-mode" nil t)
