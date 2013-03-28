f03c5ba1040688df5e14a8a1913c438c5fc4c36a
gempesaw <gempesaw@gmail.com>
2013-03-28 11:15:36 -0400

    Load all possible packages from marmalade, and use MELPA as a fallback

--- load-my-packages.el
+++ load-my-packages.el
@@ -1,61 +1,75 @@
 (require 'package)
 (require 'cl)
 (setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
-                         ;; ("melpa" . "http://melpa.milkbox.net/packages/")
                          ("marmalade" . "http://marmalade-repo.org/packages/")))
-
 (package-initialize)
 
-(defvar my-packages
-  '(
-    ace-jump-mode
-    ack-and-a-half
-    autopair
-    browse-kill-ring
-    cperl-mode
-    circe
-    dash
-    dired-details
-    elisp-slime-nav
-    exec-path-from-shell
-    expand-region
-    fetchmacs
-    gist
-    htmlize
-    impatient-mode
-    ido-ubiquitous
-    ir-black-theme
-    jabber
-    js2-mode
-    magit
-    markdown-mode
-    multiple-cursors
-    php-mode
-    regex-tool
-    scala-mode2
-    simple-httpd
-    smex
-    switch-window
-    tumblesocks
-    wgrep
-    yasnippet)
-  "A list of packages to ensure are installed at launch.")
+(defvar my-packages '() "A list of packages to ensure are installed at launch.")
+(defvar my-melpa-packages '() "A list of MELPA packages (not available on marmalade)")
 
-(defun my-packages-installed-p ()
-  (loop for p in my-packages
+(setq my-melpa-packages
+      '(
+        impatient-mode
+        ir-black-theme
+        scala-mode2
+        simple-httpd
+        ))
+
+(setq my-packages
+      '(
+        ace-jump-mode
+        ack-and-a-half
+        autopair
+        browse-kill-ring
+        cperl-mode
+        circe
+        dash
+        dired-details
+        elisp-slime-nav
+        exec-path-from-shell
+        expand-region
+        fetchmacs
+        gist
+        htmlize
+        ido-ubiquitous
+        jabber
+        js2-mode
+        magit
+        markdown-mode
+        multiple-cursors
+        php-mode
+        regex-tool
+        smex
+        switch-window
+        tumblesocks
+        wgrep
+        yasnippet))
+
+(defun my-packages-installed-p (list-of-packages)
+  (loop for p in list-of-packages
         when (not (package-installed-p p)) do (return nil)
         finally (return t)))
 
-(save-window-excursion (unless (my-packages-installed-p)
-                         ;; check for new packages (package versions)
-                         (message "%s" "Emacs Prelude is now refreshing its package database...")
-                         (package-refresh-contents)
-                         (message "%s" " done.")
-                         ;; install the missing packages
-                         (dolist (p my-packages)
-                           (when (not (package-installed-p p))
-                             (package-install p)
-                             (require p)))))
+(defun load-my-packages (list-of-packages source)
+  (let ((temporary-package-archives package-archives))
+    (save-window-excursion
+      (unless (my-packages-installed-p list-of-packages)
+        ;; check for new packages (package versions)
+        (message "%s" "Refreshing package database...")
+        (package-refresh-contents)
+        (message "%s" " done.")
+
+        (if (eq source 'melpa)
+            (setq temporary-package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
+                                     ("melpa" . "http://melpa.milkbox.net/packages/")))
+          ;; install the missing packages
+          (dolist (p list-of-packages)
+            (when (not (package-installed-p p))
+              (package-install p)
+              (require p))))))))
+
+(load-my-packages my-packages 'marmalade)
+(load-my-packages my-melpa-packages 'melpa)
 
 (package-initialize)
 
