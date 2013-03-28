6cd74f19e169533039d638ecea60f5fa2687beb3
gempesaw <gempesaw@gmail.com>
2013-03-28 10:41:51 -0400

    Remove melpa, old modes configs, general cleanup

--- load-my-packages.el
+++ load-my-packages.el
@@ -1,7 +1,7 @@
 (require 'package)
 (require 'cl)
 (setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
-                         ("melpa" . "http://melpa.milkbox.net/packages/")
+                         ;; ("melpa" . "http://melpa.milkbox.net/packages/")
                          ("marmalade" . "http://marmalade-repo.org/packages/")))
 
 (package-initialize)
