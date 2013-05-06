3d763b286ef0c5abdcaadc56c75bd57ebf604a52
gempesaw <gempesaw@gmail.com>
2013-05-06 12:11:01 -0400

    Fiddle with jabber settings

--- customize.el
+++ customize.el
@@ -170,12 +170,11 @@
 (setq bookmarks-default-file "~/.emacs.d/bookmarks")
 
 ;; jabby wabby - http://stackoverflow.com/a/5731090/1156644
-(setq jabber-account-list `(("gempesaw@gmail.com"
-                             (:password ,gmail-2-auth-pw)
-                             )
+(setq jabber-account-list `(;; ("gempesaw@gmail.com"
+                            ;;  (:password ,gmail-2-auth-pw)
+                            ;;  )
                             ("dgempesaw@sharecare.com"
-                             (:password ,jabber-password)
-                             (:connection-type . ssl))))
+                             (:connection-type . starttls))))
 (setq jabber-alert-presence-hooks nil
       jabber-avatar-verbose nil
       jabber-vcard-avatars-retrieve nil
@@ -186,8 +185,7 @@
       jabber-roster-line-format " %c %-25n %u %-8s (%r)"
       jabber-show-offline-contacts nil
       jabber-auto-reconnect t
-      jabber-muc-autojoin '("qa@conference.sharecare.com")
-      jabber-muc-default-nicknames '(("qa@conference.sharecare.com" . "Daniel Gempesaw")))
+      jabber-muc-autojoin '("qa@conference.sharecare.com"))
 (setq starttls-extra-arguments '("--insecure"))
 (setq starttls-use-gnutls t)
 
