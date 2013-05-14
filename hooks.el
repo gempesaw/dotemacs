c92ab1807608eee0a36e00b119cf0d4429d2c682
gempesaw <gempesaw@gmail.com>
2013-05-14 11:25:41 -0400

    Turn on slime nav mode for lisp projects

--- hooks.el
+++ hooks.el
@@ -38,6 +38,7 @@
 
 (add-hook 'emacs-lisp-mode-hook 'turn-on-eldoc-mode)
 (add-hook 'emacs-lisp-mode-hook 'enable-paredit-mode)
+(add-hook 'emacs-lisp-mode-hook (lambda () (elisp-slime-nav-mode t)))
 
 (add-hook 'lisp-interaction-mode-hook 'turn-on-eldoc-mode)
 
