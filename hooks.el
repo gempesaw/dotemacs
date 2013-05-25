c6ecd27893d6397361f31c44cc04c92f0979ee3c
gempesaw <gempesaw@gmail.com>
2013-05-25 18:21:43 -0400

    Add lisp hooks into eval-after-load

--- hooks.el
+++ hooks.el
@@ -31,15 +31,15 @@
             (local-set-key (kbd "<f5>") 'execute-perl)
             (hs-minor-mode t)))
 
+(eval-after-load 'paredit-mode
+  (add-hook 'emacs-lisp-mode-hook 'enable-paredit-mode))
 
-(add-hook 'mu4e-index-updated-hook
-          (defun new-mail-alert ()
-            (alert "new mail!")))
+(eval-after-load "elisp-slime-nav-mode"
+  '(progn
+     (add-hook 'emacs-lisp-mode-hook (lambda () (elisp-slime-nav-mode t)))))
 
-(add-hook 'emacs-lisp-mode-hook 'turn-on-eldoc-mode)
-(add-hook 'emacs-lisp-mode-hook 'enable-paredit-mode)
-(add-hook 'emacs-lisp-mode-hook (lambda () (elisp-slime-nav-mode t)))
-
-(add-hook 'lisp-interaction-mode-hook 'turn-on-eldoc-mode)
-
-(add-hook 'ielm-mode-hook 'turn-on-eldoc-mode)
+(eval-after-load 'eldoc
+  '(progn
+    (add-hook 'ielm-mode-hook 'turn-on-eldoc-mode)
+    (add-hook 'emacs-lisp-mode-hook 'turn-on-eldoc-mode)
+    (add-hook 'lisp-interaction-mode-hook 'turn-on-eldoc-mode)))
