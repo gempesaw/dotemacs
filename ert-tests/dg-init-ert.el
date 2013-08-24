51bc97fdcf1432c93dc5a69793539088b10e8484
Daniel Gempesaw <gempesaw@gmail.com>
2013-08-24 09:13:13 -0400

    Define freenode-password for travis

--- ert-tests/dg-init-ert.el
+++ ert-tests/dg-init-ert.el
@@ -3,6 +3,3 @@
 
 (ert-deftest dg-features-are-available ()
   (should (featurep 'flx-ido)))
-
-
-;; (compile "emacs -batch -L ~/.emacs.d/ -L /.emacs.d/elpa/ -l ert -l dg-init-ert.el -f ert-run-tests-batch-and-exit")
