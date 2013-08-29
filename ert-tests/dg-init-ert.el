4c1ea84ed505f371dae2a024de3cfb73fd02b50f
gempesaw <gempesaw@gmail.com>
2013-08-29 10:05:56 -0400

    Move all sc related defuns into their own place

--- ert-tests/dg-init-ert.el
+++ ert-tests/dg-init-ert.el
@@ -1,4 +1,5 @@
 (require 'ert)
+(require 'dg-sc-tests)
 
 (ert-deftest my-files-are-loaded ()
   (should (string= "dg-elisp-macros" (require 'dg-elisp-macros))))
---
+++ ert-tests/dg-sc-ert.el
@@ -0,0 +1,11 @@
+(require 'ert)
+
+(ert-deftest decide-what-to-restart ()
+  (should (sc--box-in-restart-group-p "scqaschedulemaster2f" "all"))
+  (should (not (sc--box-in-restart-group-p "scqaschedulemaster2f" "half")))
+  (should (not (sc--box-in-restart-group-p "scqaschedulemaster2f" "pubs")))
+  (should (sc--box-in-restart-group-p "scqawebauth2f" "all"))
+  (should (sc--box-in-restart-group-p "scqawebauth2f" "half"))
+  (should (not (sc--box-in-restart-group-p "scqawebauth2f" "pubs"))))
+
+(provide 'dg-sc-tests)
