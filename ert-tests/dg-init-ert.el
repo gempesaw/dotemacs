53d85008489048fc13dcc23956da2bf7fb49da27
gempesaw <gempesaw@gmail.com>
2013-08-29 10:25:44 -0400

    Try to convince travis not to download packages

--- ert-tests/dg-init-ert.el
+++ ert-tests/dg-init-ert.el
@@ -1,3 +1,9 @@
+(add-to-list 'load-path "~/.emacs.d/")
+(add-to-list 'load-path "~/.emacs.d/elpa/")
+(package-initialize)
+
+(message (mapconcat (lambda (it) (symbol-name (car it))) package-alist " "))
+
 (require 'ert)
 (require 'dg-sc-tests)
 
