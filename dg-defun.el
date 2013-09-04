f1532851743fecfdecf0e1fbc924236c7742a950
gempesaw <gempesaw@gmail.com>
2013-09-04 10:47:24 -0400

    Remove duplicate defun

--- dg-defun.el
+++ dg-defun.el
@@ -646,8 +646,4 @@ Return nil."
     (delete-window window))
   nil)
 
-(defun ert-my-dotemacs ()
-  (interactive)
-  (compile "cd ~/.emacs.d/ && pwd && emacs --version && emacs -batch -L ./ -L ./elpa/ -L ./ert-tests/ -l ert -l ./ert-tests/dg-init-ert.el -f ert-run-tests-batch-and-exit"))
-
 (provide 'dg-defun)
