ce871cf467da7f0a7f64ec12b07d5090d7cfffc1
gempesaw <gempesaw@gmail.com>
2013-09-06 11:29:34 -0400

    Add pubs and army only selection

--- ert-tests/dg-sc-ert.el
+++ ert-tests/dg-sc-ert.el
@@ -7,6 +7,8 @@
   (should (not (sc--box-in-restart-group-p "scqaschedulemaster2f" "pubs")))
   (should (sc--box-in-restart-group-p "scqawebauth2f" "all"))
   (should (sc--box-in-restart-group-p "scqawebauth2f" "half"))
-  (should (not (sc--box-in-restart-group-p "scqawebauth2f" "pubs"))))
+  (should (not (sc--box-in-restart-group-p "scqawebauth2f" "pubs")))
+  (should (not (sc--box-in-restart-group-p "scqawebauth2f" "pub")))
+  (should (sc--box-in-restart-group-p "scqawebpub2f" "pub")))
 
 (provide 'dg-sc-ert)
