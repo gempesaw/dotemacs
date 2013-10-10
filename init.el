32d69100dd958d1024f37c6ad29288771d7078fe
gempesaw <gempesaw@gmail.com>
2013-10-10 11:11:47 -0400

    Search for my dg- packages automatically

--- init.el
+++ init.el
@@ -1,28 +1,21 @@
 (add-to-list 'load-path "~/.emacs.d/")
 ;; (byte-recompile-directory (expand-file-name "~/.emacs.d/elpa/") 0 t)
 
-(setq dg-files '(
-                 dg-load-my-packages    ;; load all the necessary packages
-                 dg-override-keys       ;; needs to be first so other files can use it
-                 dg-passwords           ;; needs to be first so other files can use it
-                 dg-defun
-                 dg-elisp-macros
-                 dg-fullscreen
-                 dg-jabber
-                 dg-js
-                 dg-kbd
-                 dg-minibuffer
-                 dg-misc
-                 dg-mu4e
-                 dg-numpad
-                 dg-sc
-                 dg-selenium
-                 dg-tabs
-                 dg-tramp
-                 ))
+(setq dg-files
+      (delq nil (delete-dups              ;; remove duplicates
+                 (append
+                  '(
+                    "dg-load-my-packages" ;; load melpa/marmalade packages
+                    "dg-override-keys"    ;; needs to be first so other files can use it
+                    "dg-passwords"        ;; needs to be first so other files can use it
+                    )
+                  (mapcar
+                   (lambda (it)           ;; and append any dg-* files in this folder
+                     (substring it 0 -3))
+                   (file-expand-wildcards "dg-*.el"))))))
 
 (mapcar (lambda (it)
-          (if (file-exists-p (concat "~/.emacs.d/" (symbol-name it) ".el"))
+          (if (file-exists-p (concat "~/.emacs.d/" it ".el"))
               (progn (message "    ****Loading %s****    " it)
                      (require it))
             (message "____****MISSING: %s****____" it)))
