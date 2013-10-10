c7cc8c8201e6d148d4ad4834bc0149cbee0120c3
gempesaw <gempesaw@gmail.com>
2013-10-10 11:15:49 -0400

    Add useful keychords

--- dg-kbd.el
+++ dg-kbd.el
@@ -218,11 +218,14 @@
     (key-chord-define-global "qq" 'window-configuration-to-register)
     (key-chord-define-global "xj" 'jump-to-register)
     (key-chord-define-global "xf" 'find-file)
+    (key-chord-define-global "xr" 'find-file-as-root)
     (key-chord-define-global "xd" '[?\C-x ?d return])
     (key-chord-define-global "xb" 'ido-switch-buffer)
     (key-chord-define-global "xv" 'switch-to-other-buffer)
     (key-chord-define-global "xh" 'mark-whole-buffer)
-    (key-chord-define-global "cj" (lambda ()
+    (key-chord-define-global "jk" 'eval-defun)
+    (key-chord-define-global "jl" 'jabber-activity-switch-to)
+    (key-chord-define-global "bj" (lambda ()
                                     (interactive)
                                     (bookmark-jump
                                      (ido-completing-read
