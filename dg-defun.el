7c31d999143fd6b4f80dd43d9b1e5cd18c91da57
gempesaw <gempesaw@gmail.com>
2013-10-10 11:15:33 -0400

    Use find-file-as-root as direct-to-sudo

--- dg-defun.el
+++ dg-defun.el
@@ -363,8 +363,7 @@ root-privileges (using tramp/sudo), if the file is not writable
 by user."
   (interactive)
   (let ((file (ido-read-file-name "Edit as root: ")))
-    (unless (file-writable-p file)
-      (setq file (concat "/sudo:root@localhost:" file)))
+    (setq file (concat "/sudo:root@localhost:" file))
     (find-file file)))
 
 ;; http://ergoemacs.org/emacs/elisp_delete-current-file.html
