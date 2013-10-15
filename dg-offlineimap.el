4f613d6ab1906f9f99b34a9eaa17dd94bc91042a
gempesaw <gempesaw@gmail.com>
2013-10-15 19:36:33 -0400

    Try harder to kill offlineimap before restarting it

--- dg-offlineimap.el
+++ dg-offlineimap.el
@@ -1,7 +1,9 @@
 (defun offlineimap-rudely-restart ()
   (interactive)
   (shell-command "pkill -u dgempesaw -s 9 -f offlineimap")
+  (kill-buffer "*OfflineIMAP*")
   (offlineimap-quit)
+  (sit-for 0)
   (offlineimap))
 
 (provide 'dg-offlineimap)
