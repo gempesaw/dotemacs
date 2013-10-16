1461acc3dbba1c4153bbb2f5139f7f29bf052af5
gempesaw <gempesaw@gmail.com>
2013-10-16 10:14:56 -0400

    Consolidate os x settings

---
+++ dg-os-x.el
@@ -0,0 +1,12 @@
+;; make OS X command button meta
+(setq mac-option-key-is-meta nil)
+(setq mac-command-key-is-meta t)
+(setq mac-command-modifier 'meta)
+
+;; oh cool we get a super button!
+(setq mac-option-modifier 'super)
+
+;;; don't use the os x horrible fullscreen method
+(setq ns-use-native-fullscreen nil)
+
+(provide 'dg-os-x)
