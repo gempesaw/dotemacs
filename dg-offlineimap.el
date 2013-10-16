604edd274dacd44e972f240a23a6a9117de8cd40
gempesaw <gempesaw@gmail.com>
2013-10-16 10:16:00 -0400

    Consolidate offlineimap settings

--- dg-offlineimap.el
+++ dg-offlineimap.el
@@ -6,4 +6,13 @@
   (sit-for 0)
   (offlineimap))
 
+;; offlineimappppy
+(setq offlineimap-buffer-maximum-size 256
+      offlineimap-command "offlineimap -u machineui"
+      offlineimap-enable-mode-line-p (member major-mode
+                                             '(offlineimap-mode
+                                               gnus-group-mode
+                                               mu4e-headers-mode
+                                               mu4e-view-mode)))
+
 (provide 'dg-offlineimap)
