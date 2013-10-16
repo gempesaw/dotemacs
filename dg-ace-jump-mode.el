6653344fbdf39642dfc6d114fd552428569f7686
gempesaw <gempesaw@gmail.com>
2013-10-15 20:43:56 -0400

    Consolidate ace-jump-mode settings

--- dg-ace-jump-mode.el
+++ dg-ace-jump-mode.el
@@ -1,1 +1,8 @@
+(setq ace-jump-mode-scope 'frame)
+(define-key my-keys-minor-mode-map (kbd "C-.") 'ace-jump-mode)
+
+(eval-after-load "tracking"
+  '(progn
+     (define-key tracking-mode-map (kbd "C-.") 'ace-jump-mode)))
+
 (provide 'dg-ace-jump-mode)
