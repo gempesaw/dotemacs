3acdecb2db13f1f83400bb0d5c5b18a0af9c164c
gempesaw <gempesaw@gmail.com>
2013-10-15 20:24:20 -0400

    Load packages with file-expand-wildcards at long last

--- init.el
+++ init.el
@@ -1,24 +1,23 @@
 (add-to-list 'load-path "~/.emacs.d/")
 ;; (byte-recompile-directory (expand-file-name "~/.emacs.d/elpa/") 0 t)
 
+(setq dg-files '(
+                 "dg-load-my-packages" ;; load melpa/marmalade packages
+                 "dg-override-keys"    ;; needs to be first so other files can use it
+                 ))
+
+(cd "~/")
 (setq dg-files
-      (delq nil (delete-dups              ;; remove duplicates
-                 (append
-                  '(
-                    "dg-load-my-packages" ;; load melpa/marmalade packages
-                    "dg-override-keys"    ;; needs to be first so other files can use it
-                    "dg-passwords"        ;; needs to be first so other files can use it
-                    )
-                  (mapcar
-                   (lambda (it)           ;; and append any dg-* files in this folder
-                     (substring it 0 -3))
-                   (file-expand-wildcards "dg-*.el"))))))
+      (delq nil
+            (delete-dups (append dg-files
+                                 (mapcar (lambda (it) (substring it 9 -3))
+                                         (file-expand-wildcards ".emacs.d/dg-*.el"))))))
 
 (mapcar (lambda (it)
-          (if (file-exists-p (concat "~/.emacs.d/" it ".el"))
-              (progn (message "    ****Loading %s****    " it)
-                     (require it))
-            (message "____****MISSING: %s****____" it)))
+          (message (if (require (intern it))
+                       "    ****Loading %s****    "
+                     "____****MISSING: %s****____")
+                   it))
         dg-files)
 
 (load "customize.el" 'noerror)
@@ -28,5 +27,3 @@
 (load "hooks.el" 'noerror)
 (load "modes.el" 'noerror)
 (load "my-macros.el" 'noerror)
-
-(provide 'init)
