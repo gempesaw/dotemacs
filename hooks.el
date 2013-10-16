6b049513c0f1207ce31ac39702132fd25f14902d
gempesaw <gempesaw@gmail.com>
2013-10-15 20:48:56 -0400

    Consolidate emacs-lisp-mode settings

--- hooks.el
+++ hooks.el
@@ -20,21 +20,6 @@
             (local-set-key (kbd "<f5>") 'execute-perl)
             (hs-minor-mode t)))
 
-(eval-after-load 'paredit
-  (autoload 'enable-paredit-mode "paredit"
-    "Turn on pseudo-structural editing of Lisp code."
-    t))
-
-(eval-after-load "elisp-slime-nav"
-  '(progn
-     (add-hook 'emacs-lisp-mode-hook (lambda () (elisp-slime-nav-mode t)))))
-
-(eval-after-load "eldoc"
-  '(progn
-     (add-hook 'ielm-mode-hook 'turn-on-eldoc-mode)
-     (add-hook 'emacs-lisp-mode-hook 'turn-on-eldoc-mode)
-     (add-hook 'lisp-interaction-mode-hook 'turn-on-eldoc-mode)))
-
 ;; (setq mu4e-index-updated-hook nil)
 (add-hook 'mu4e-index-updated-hook
           (lambda ()
