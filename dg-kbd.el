145fa51b294b5bf8f4ee6e31a22e1b184b3f610f
gempesaw <gempesaw@gmail.com>
2013-10-24 12:13:55 -0400

    `s-l`: lift this thing. `s-p`: pull from down there.

--- dg-kbd.el
+++ dg-kbd.el
@@ -235,7 +235,8 @@
     (define-key php-mode-map (kbd "<tab>") 'smart-tab)
     nil))
 
-(global-set-key (kbd "C-M-^") (lambda () (interactive) (delete-indentation -1)))
+(global-set-key (kbd "s-l") 'delete-indentation)
+(global-set-key (kbd "s-p") (lambda () (interactive) (delete-indentation -1)))
 
 (global-set-key (kbd "s-k") 'delete-window-and-kill-buffer)
 (global-set-key (kbd "C-c k") (lambda () (interactive) (delete-other-window t)))
