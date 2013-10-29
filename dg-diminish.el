2b46c4f9f30dbc67c18fea1a7123987518b4a214
gempesaw <gempesaw@gmail.com>
2013-10-29 11:35:37 -0400

    Be slightly smarter about diminishing things

--- dg-diminish.el
+++ dg-diminish.el
@@ -1,12 +1,15 @@
-(mapc (lambda (it) (diminish it))
-      '(eldoc-mode
-        elisp-slime-nav-mode
-        paredit-mode
-        projectile-mode
-        smart-tab-mode
-        subword-mode
-        wrap-region-mode
-        yas-minor-mode
-        my-keys-minor-mode))
+(mapc (lambda (it)
+        (eval-after-load (car it)
+          '(diminish (cadr it))))
+      '(("eldoc" eldoc-mode
+         "elisp-slime-nav" elisp-slime-nav-mode
+         "paredit" paredit-mode
+         "projectile" projectile-mode
+         "smart-tab" smart-tab-mode
+         "subword" subword-mode
+         "wrap-region" wrap-region-mode
+         "yas-minor" yas-minor-mode
+         "my-keys-minor" my-keys-minor-mode
+         )))
 
 (provide 'dg-diminish)
