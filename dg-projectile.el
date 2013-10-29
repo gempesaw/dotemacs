a1ede1d2ad555b4ed95451a4a8fe2740df616e02
gempesaw <gempesaw@gmail.com>
2013-10-29 11:24:38 -0400

    Use projectile mode

---
+++ dg-projectile.el
@@ -0,0 +1,8 @@
+(define-key projectile-mode-map [?\s-d] 'projectile-find-dir)
+(define-key projectile-mode-map [?\s-p] 'projectile-switch-project)
+(define-key projectile-mode-map [?\s-f] 'projectile-find-file)
+(define-key projectile-mode-map [?\s-g] 'projectile-grep)
+
+(projectile-global-mode 1)
+
+(provide 'dg-projectile)
