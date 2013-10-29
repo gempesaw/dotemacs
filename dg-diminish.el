ac316913ede8b16c317f03317bd95000eacb1fb3
gempesaw <gempesaw@gmail.com>
2013-10-29 11:24:25 -0400

    Diminish things! finally :)

--- dg-diminish.el
+++ dg-diminish.el
@@ -1,1 +1,12 @@
+(mapc (lambda (it) (diminish it))
+      '(eldoc-mode
+        elisp-slime-nav-mode
+        paredit-mode
+        projectile-mode
+        smart-tab-mode
+        subword-mode
+        wrap-region-mode
+        yas-minor-mode
+        my-keys-minor-mode))
+
 (provide 'dg-diminish)
