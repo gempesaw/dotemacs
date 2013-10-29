78f1a8e93c1f5791df13db0cad434039d658f735
gempesaw <gempesaw@gmail.com>
2013-10-29 10:27:08 -0400

    Update melpa packages

--- elpa/ido-ubiquitous-20130921.1022/ido-ubiquitous-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "ido-ubiquitous" "20130921.1022" "Use ido (nearly) everywhere. [github]" '((emacs "24.1")))
---
+++ elpa/jabber-20131018.2132/jabber-pkg.el
@@ -0,0 +1,1 @@
+(define-package "jabber" "20131018.2132" "A Jabber client for Emacs. [git]" 'nil)
