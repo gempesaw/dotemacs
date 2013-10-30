5252c2372432a43bb3ddd5fff0a723cb1c267f83
gempesaw <gempesaw@gmail.com>
2013-10-30 09:53:51 -0400

    Limit what symbols get prettified by pretty-mode-plus

--- dg-pretty-mode-plus.el
+++ dg-pretty-mode-plus.el
@@ -1,3 +1,17 @@
+(setq pretty-default-groups
+  '(:function
+    :equality
+    :nil
+    ))
+
+;; (setq pretty-supported-modes
+;;   '(ruby-mode
+;;     ess-mode java-mode octave-mode tuareg-mode
+;;     python-mode sml-mode jess-mode clips-mode clojure-mode
+;;     lisp-mode emacs-lisp-mode scheme-mode sh-mode
+;;     perl-mode c++-mode c-mode haskell-mode
+;;     javascript-mode coffee-mode groovy-mode))
+
 (global-pretty-mode 1)
 
 (provide 'dg-pretty-mode-plus)
