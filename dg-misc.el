9d5c73f73cfc2d2fad162968a511b25a824af77f
gempesaw <gempesaw@gmail.com>
2013-11-01 12:05:30 -0400

    Make testing functions use newest version

--- dg-misc.el
+++ dg-misc.el
@@ -1,5 +1,9 @@
 (defun dg-test-dotemacs ()
   (interactive)
-  (compile "cd ~/.emacs.d/ && which emacs && emacs -batch -L ./ -L ./elpa/ -L ./ert-tests/ -l ert -l ./ert-tests/dg-init-ert.el -f ert-run-tests-batch-and-exit"))
+  (compile "cd ~/.emacs.d/ && /Applications/Emacs.app/Contents/MacOS/Emacs -batch -L ./ -L ./elpa/ -L ./ert-tests/ -l ert -l ./ert-tests/dg-init-ert.el -f ert-run-tests-batch-and-exit"))
+
+(defun dg-test-dotemacs-interactively ()
+  (interactive)
+  (compile "cd ~/.emacs.d/ && /Applications/Emacs.app/Contents/MacOS/Emacs"))
 
 (provide 'dg-misc)
