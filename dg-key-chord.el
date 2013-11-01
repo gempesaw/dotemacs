eef26b7449b2ded02fe1413264a10ce26dd47e57
gempesaw <gempesaw@gmail.com>
2013-11-01 12:04:10 -0400

    Move keychords into their proper customization file

--- dg-key-chord.el
+++ dg-key-chord.el
@@ -1,1 +1,24 @@
+(eval-after-load 'key-chord
+  (progn
+    (key-chord-mode t)
+    (key-chord-define-global "xw" 'ido-write-file)
+    (key-chord-define-global "xg" 'magit-status)
+    (key-chord-define-global "qq" 'window-configuration-to-register)
+    (key-chord-define-global "xj" 'jump-to-register)
+    (key-chord-define-global "xf" 'find-file)
+    (key-chord-define-global "xr" 'find-file-as-root)
+    (key-chord-define-global "xd" '[?\C-x ?d return])
+    (key-chord-define-global "xb" 'ido-switch-buffer)
+    (key-chord-define-global "xv" 'switch-to-other-buffer)
+    (key-chord-define-global "xh" 'mark-whole-buffer)
+    (key-chord-define-global "jk" 'eval-defun)
+    (key-chord-define-global "jl" 'jabber-activity-switch-to)
+    (key-chord-define-global "bj" (lambda ()
+                                    (interactive)
+                                    (bookmark-jump
+                                     (ido-completing-read
+                                      "Jump to bookmark: "
+                                      (bookmark-all-names)))))
+    nil))
+
 (provide 'dg-key-chord)
