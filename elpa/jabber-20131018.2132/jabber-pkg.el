4b2aa7de890971311dd73dd76ff8930b123b075e
gempesaw <gempesaw@gmail.com>
2013-11-13 22:13:35 -0500

    Update packages (not ido-vertical or magit)

--- elpa/jabber-20131018.2132/jabber-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "jabber" "20131018.2132" "A Jabber client for Emacs. [git]" 'nil)
--- elpa/regex-tool-20130328.1510/regex-tool-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "regex-tool" "20130328.1510" "A regular expression evaluation tool for programmers [github]" (quote nil))
