b021d045978957a149d1c7bea0b0c29d512349d9
gempesaw <gempesaw@gmail.com>
2013-11-13 21:15:59 -0500

    Update package: ido-vertical-mode

--- elpa/ido-vertical-mode-0.1.1/ido-vertical-mode-autoloads.el
+++ elpa/ido-vertical-mode-0.1.1/ido-vertical-mode-autoloads.el
@@ -4,7 +4,7 @@
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
 ;;;### (autoloads nil "ido-vertical-mode" "ido-vertical-mode.el"
-;;;;;;  (21103 51395 0 0))
+;;;;;;  (21124 12453 0 0))
 ;;; Generated autoloads from ido-vertical-mode.el
 
 (defvar ido-vertical-mode nil "\
--- elpa/ido-vertical-mode-20130422.1226/ido-vertical-mode-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "ido-vertical-mode" "20130422.1226" "Makes ido-mode display vertically. [github]" 'nil)
