6ffe875703c248928ce5998c5c787d3de7720b67
gempesaw <gempesaw@gmail.com>
2014-01-31 10:32:37 -0500

    Use web mode for php and html files

--- dg-web-mode.el
+++ dg-web-mode.el
@@ -1,3 +1,5 @@
+(require 'web-mode)
+
 (defvar dg-web-mode-indent 4
   "I like 4 spaces instead of 2.")
 
@@ -9,5 +11,13 @@
         web-mode-code-indent-offset 4))
 
 (add-hook 'web-mode-hook  'web-mode-hook)
+(add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.php[s345t]?\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("Amkfile" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.amk$" . web-mode))
+
 
 (provide 'dg-web-mode)
