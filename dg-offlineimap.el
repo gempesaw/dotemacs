9fb06c31ce8130652ce41fb02fa2a03d749e4115
gempesaw <gempesaw@gmail.com>
2014-01-31 10:33:06 -0500

    Remove offlineimap hard reset from its window functions

--- dg-offlineimap.el
+++ dg-offlineimap.el
@@ -1,10 +1,15 @@
-(defun offlineimap-rudely-restart ()
+(defun offlineimap-kill ()
   (interactive)
-  (offlineimap-quit)
-  (shell-command "pkill -u dgempesaw -s 9 -f offlineimap")
-  (kill-buffer "*OfflineIMAP*")
-  (sit-for 1)
-  (offlineimap))
+  (save-window-excursion
+    (shell-command "pkill -u dgempesaw -s 9 -f offlineimap")
+    (kill-buffer (get-buffer "*OfflineIMAP*"))))
+
+(defun offlineimap-show-or-start ()
+  (interactive)
+  (if (switch-between-buffers "*OfflineIMAP*")
+      (if (get-buffer-process (current-buffer))
+          (goto-char (point-max))
+        (offlineimap))))
 
 ;; offlineimappppy
 (setq offlineimap-buffer-maximum-size 256
@@ -15,14 +20,9 @@
                                                mu4e-headers-mode
                                                mu4e-view-mode)))
 
+
 (global-unset-key (kbd "s-o"))
-(global-set-key (kbd "s-o") '(lambda () (interactive)
-                               (if (switch-between-buffers "*OfflineIMAP*")
-                                   (if (get-buffer-process (current-buffer))
-                                       (progn
-                                         (goto-char (point-max))
-                                         (offlineimap-resync))
-                                     (offlineimap)))))
-(global-set-key (kbd "s-0") 'offlineimap-rudely-restart)
+(global-set-key (kbd "s-o") 'offlineimap-show-or-start)
+(global-set-key (kbd "s-0") 'offlineimap-kill)
 
 (provide 'dg-offlineimap)
