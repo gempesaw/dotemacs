584ded95b156b46eedcc0e3552c5785f1ea38afc
gempesaw <gempesaw@gmail.com>
2014-03-27 10:22:23 -0400

    Ignore more things when grepping

--- dg-elisp/dg-grep.el
+++ dg-elisp/dg-grep.el
@@ -1,3 +1,18 @@
 (grep-apply-setting 'grep-find-command (cons "find . -type f \\! -name \"ido.last*\" \\! -path \"*git*\" \\! -path \"*elpa*\" \\! -path \"*backups*\" -exec grep -nH -e  {} +" 111))
 
+(setq grep-find-ignored-directories '("app/bower_components"
+                                      "node_modules"
+                                      "SCCS"
+                                      "RCS"
+                                      "CVS"
+                                      "MCVS"
+                                      ".svn"
+                                      ".git"
+                                      ".hg"
+                                      ".bzr"
+                                      "_MTN"
+                                      "_darcs"
+                                      "{arch}"))
+
+
 (provide 'dg-grep)
