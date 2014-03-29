5a9858d54b989bf54bc2f82fee59b100bd3f0947
Daniel Gempesaw <gempesaw@gmail.com>
2014-03-29 09:44:55 -0400

    Rely on relative path resolution for wd-manager

--- dg-elisp/dg-selenium.el
+++ dg-elisp/dg-selenium.el
@@ -4,7 +4,7 @@
          (selenium-buffer (concat "*" selenium-proc-name "-2.40.0*")))
     (when (eq nil (get-buffer selenium-buffer))
       (set-process-query-on-exit-flag
-       (start-process selenium-proc-name selenium-buffer "/usr/local/share/npm/bin/webdriver-manager" "start")
+       (start-process selenium-proc-name selenium-buffer "webdriver-manager" "start")
        nil)
       (switch-to-buffer selenium-buffer)
       (setq buffer-read-only t))))
