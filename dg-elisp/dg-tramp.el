905ae3a55aa2a28e738252ffb230d176481c080a
Daniel Gempesaw <gempesaw@gmail.com>
2014-04-01 10:34:03 -0400

    Add hack to convince tramp to ignore invalid gid

--- dg-elisp/dg-tramp.el
+++ dg-elisp/dg-tramp.el
@@ -5,6 +5,30 @@
 ;; https://github.com/gwtaylor/dotfiles/blob/master/.emacs.d/darwin.el
 (setenv "TMPDIR" "/tmp/")
 
+(defun tramp-send-command-and-read (vec command &optional noerror)
+  "Run COMMAND and return the output, which must be a Lisp expression.
+In case there is no valid Lisp expression and NOERROR is nil, it
+raises an error."
+  (when (string= command "/usr/bin/id -gn | sed -e s/^/\\\"/ -e s/$/\\\"/")
+    (setq command "echo '514'"))
+  (when (if noerror
+            (tramp-send-command-and-check vec command)
+          (tramp-barf-unless-okay
+           vec command "`%s' returns with error" command))
+    (with-current-buffer (tramp-get-connection-buffer vec)
+      ;; Read the expression.
+      (goto-char (point-min))
+      (condition-case nil
+          (prog1 (read (current-buffer))
+            ;; Error handling.
+            (when (re-search-forward "\\S-" (point-at-eol) t)
+              (error nil)))
+        (error (unless noerror
+                 (tramp-error
+                  vec 'file-error
+                  "`%s' does not return a valid Lisp expression: `%s'"
+                  command (buffer-string))))))))
+
 ;; tramp settings
 (eval-after-load "tramp"
   '(progn
