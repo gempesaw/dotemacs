857cfb60e6845f82713e208eefaeec018b54d6b0
Daniel Gempesaw <gempesaw@gmail.com>
2014-04-12 09:00:22 -0400

    Reorganize key chords courtesy of http://www.reddit.com/r/emacs/comments/22hzx7/what_are_your_keychord_abbreviations/

--- dg-elisp/dg-key-chord.el
+++ dg-elisp/dg-key-chord.el
@@ -1,26 +1,50 @@
 (eval-after-load 'key-chord
   (progn
+
+    (setq key-chord-two-keys-delay .1
+          key-chord-one-key-delay .2)
+
     (key-chord-mode t)
-    (key-chord-define-global "xw" 'ido-write-file)
+
+    ;; movement, shells, smex
+    (key-chord-define-global "./" 'ace-jump-mode)
+    (key-chord-define-global ",." 'switch-to-shell-or-create)
+    (key-chord-define-global ",/" 'smex)
+
+
+    ;; registers
     (key-chord-define-global "xg" 'magit-status)
     (key-chord-define-global "qq" 'window-configuration-to-register)
     (key-chord-define-global "xj" 'jump-to-register)
-    (key-chord-define-global "xf" 'find-file)
-    (key-chord-define-global "xr" 'find-file-as-root)
-    (key-chord-define-global "xd" '[?\C-x ?d return])
-    (key-chord-define-global "xb" 'ido-switch-buffer)
-    (key-chord-define-global "xv" 'switch-to-other-buffer)
-    (key-chord-define-global "xh" 'mark-whole-buffer)
-    (key-chord-define-global "jk" 'eval-defun)
-    (key-chord-define-global "fd" 'edebug-defun)
-    (key-chord-define-global "jl" 'jabber-activity-switch-to)
-    (key-chord-define-global "xm" (lambda () (interactive) (switch-to-buffer "*compilation*")))
-    (key-chord-define-global "bj" (lambda ()
+    (key-chord-define-global "j." (lambda ()
                                     (interactive)
                                     (bookmark-jump
                                      (ido-completing-read
                                       "Jump to bookmark: "
                                       (bookmark-all-names)))))
+
+    ;; files
+    (key-chord-define-global "xw" 'ido-write-file)
+    (key-chord-define-global "xf" 'find-file)
+    (key-chord-define-global "xr" 'find-file-as-root)
+    (key-chord-define-global "xd" '[?\C-x ?d return])
+
+    ;; buffers
+    (key-chord-define-global "xb" 'ido-switch-buffer)
+    (key-chord-define-global "xv" 'switch-to-other-buffer)
+    (key-chord-define-global "xh" 'mark-whole-buffer)
+    (key-chord-define-global "xm" (lambda () (interactive) (switch-to-buffer "*compilation*")))
+
+    ;; elisp
+    (key-chord-define emacs-lisp-mode-map "jk" 'eval-defun)
+    (key-chord-define emacs-lisp-mode-map "fd" 'edebug-defun)
+
+    ;; jabber & activity
+    (key-chord-define-global "jl" 'jabber-activity-switch-to)
+    (key-chord-define-global "zx" 'jabber-connect-all)
+    (key-chord-define-global "zc" 'jabber-chat-with)
+    (key-chord-define-global "hk" 'tracking-next-buffer)
+
     nil))
 
 (provide 'dg-key-chord)
