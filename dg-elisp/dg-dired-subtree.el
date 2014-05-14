4927cc0e7318fadbdbdda0b75c5005c9c155dfff
Daniel Gempesaw <gempesaw@gmail.com>
2014-05-14 15:55:39 -0400

    Use dired-subtree and put it on "i" and "k"

---
+++ dg-elisp/dg-dired-subtree.el
@@ -0,0 +1,14 @@
+(define-key dired-mode-map (kbd "i")
+  (lambda ()
+    (interactive)
+    (dired-subtree-insert)
+    (revert-buffer)))
+
+
+(define-key dired-mode-map (kbd "k")
+  (lambda ()
+    (interactive)
+    (dired-subtree-remove)
+    (revert-buffer)))
+
+(provide 'dg-dired-subtree)
---
+++ elpa/dired-hacks-utils-20140401.1220/dired-hacks-utils-pkg.el
@@ -0,0 +1,1 @@
+(define-package "dired-hacks-utils" "20140401.1220" "Utilities and helpers for dired-hacks collection" '((dash "2.5.0")) :keywords ("files"))
