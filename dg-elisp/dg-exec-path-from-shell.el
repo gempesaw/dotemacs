8fc1b78e3cc3020139846783041c926d8b4bd778
Daniel Gempesaw <gempesaw@gmail.com>
2014-07-05 08:29:42 -0400

    Catch wrong-type-error when PERL5LIB env var doesn't exist

--- dg-elisp/dg-exec-path-from-shell.el
+++ dg-elisp/dg-exec-path-from-shell.el
@@ -1,6 +1,7 @@
 (setenv "PATH" (concat (getenv "PATH") ":/usr/local/bin"))
 (setenv "NODE_PATH" (concat (getenv "NODE_PATH") "/Usr/Local/lib/node_modules"))
 (setq exec-path (append exec-path '("/usr/local/bin")))
-(exec-path-from-shell-copy-env "PERL5LIB")
+(when (getenv "PERL5LIB")
+  (exec-path-from-shell-copy-env "PERL5LIB"))
 
 (provide 'dg-exec-path-from-shell)
