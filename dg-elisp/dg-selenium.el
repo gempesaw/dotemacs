8911c08a0f92d7703dabd9904b89e51eb3e50f7f
Daniel Gempesaw <gempesaw@gmail.com>
2014-07-16 13:38:36 -0400

    Update selenium startup scripts

--- dg-elisp/dg-selenium.el
+++ dg-elisp/dg-selenium.el
@@ -13,13 +13,36 @@
 (defun start-appium-server ()
   (interactive)
   (let* ((appium-proc-name "appium")
-         (appium-buffer (concat "*" appium-proc-name "-0.17.6*")))
+         (appium-buffer (concat "*" appium-proc-name "*")))
     (with-current-buffer (get-buffer-create appium-buffer)
       (comint-mode)
       (set-process-query-on-exit-flag
-       (start-process appium-proc-name appium-buffer "appium" "--log-no-colors")
+       (start-process appium-proc-name appium-buffer
+                      "appium"
+                      "--avd"
+                      "appium"
+                      "--full-reset"
+                      "--log-no-colors")
+       nil))
+    (switch-to-buffer appium-buffer)))
+
+(if (executable-find "protractor")
+    (start-selenium-server))
+
+(defun start-browsermob-proxy ()
+  (interactive)
+  (let* ((bmp-proc-name "browsermob-proxy")
+         (bmp-executable (executable-find "browsermob-proxy"))
+         (default-directory (f-dirname bmp-executable))
+         (buf (format "*%s*<bmp>" bmp-proc-name)))
+    (with-current-buffer (get-buffer-create buf)
+      (comint-mode)
+      (set-process-query-on-exit-flag
+       (start-process bmp-proc-name buf "browsermob-proxy")
        nil))))
 
-(start-selenium-server)
+(defun reset-emulator ()
+  (interactive)
+  (async-shell-command "sh /Users/dgempesaw/opt/AndroidSC/emulator.sh"))
 
 (provide 'dg-selenium)
