89104c73afdd7ff78fcaafad1276741c1c7135d7
Daniel Gempesaw <gempesaw@gmail.com>
2014-07-16 13:34:15 -0400

    Update packages (from a while ago)

--- elpa/grunt-20140517.840/grunt-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "grunt" "20140517.840" "Some glue to stick Emacs and Gruntfiles together" '((dash "2.6.0")) :url "https://github.com/gempesaw/grunt.el" :keywords ("convenience" "grunt"))
--- elpa/simple-httpd-20140224.631/simple-httpd-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "simple-httpd" "20140224.631" "pure elisp HTTP server" '((cl-lib "0.3")))
