5e820be8b9844e48e6293e5ec0d4061132ddc400
Daniel Gempesaw <gempesaw@gmail.com>
2014-08-28 10:36:48 -0400

    Give up on marmalade

--- dg-elisp/dg-load-my-packages.el
+++ dg-elisp/dg-load-my-packages.el
@@ -3,7 +3,8 @@
 (require 'tramp)
 (setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
                          ("melpa" . "http://melpa.milkbox.net/packages/")
-                         ("marmalade" . "http://marmalade-repo.org/packages/")))
+                         ;; ("marmalade" . "http://marmalade-repo.org/packages/")
+                         ))
 
 (package-initialize)
 
