c0ec7b6d51aa6fe30b522b177f4fbe67ce26618a
Daniel Gempesaw <gempesaw@gmail.com>
2014-10-27 14:53:02 -0400

    Update wgrep, failing to solve stringp = nil issue

--- elpa/wgrep-20140420.446/wgrep-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "wgrep" "20140420.446" "Writable grep buffer and apply the changes to files" 'nil :url "http://github.com/mhayashi1120/Emacs-wgrep/raw/master/wgrep.el" :keywords ("grep" "edit" "extensions"))
--- elpa/wgrep-20141016.1656/wgrep-autoloads.el
+++ elpa/wgrep-20141016.1656/wgrep-autoloads.el
@@ -3,13 +3,14 @@
 ;;; Code:
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads nil "wgrep" "wgrep.el" (21368 14566 0 0))
+;;;### (autoloads nil "wgrep" "wgrep.el" (21576 3096 0 0))
 ;;; Generated autoloads from wgrep.el
 
 (autoload 'wgrep-setup "wgrep" "\
 Setup wgrep preparation.
 
 \(fn)" nil nil)
+
 (add-hook 'grep-setup-hook 'wgrep-setup)
 
 ;;;***
