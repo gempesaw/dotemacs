33b59b3f7f5495371cfa0ff91809b67e07babe2b
Daniel Gempesaw <gempesaw@gmail.com>
2014-10-27 14:55:51 -0400

    Set up path for emacs shells

--- dg-elisp/dg-selenium.el
+++ dg-elisp/dg-selenium.el
@@ -46,3 +46,5 @@
   (async-shell-command "sh /Users/dgempesaw/opt/AndroidSC/emulator.sh"))
 
 (provide 'dg-selenium)
+
+(setenv "PATH" "/usr/local/bin:/opt/adt-bundle-mac-x86_64-20140321/sdk/tools:/opt/adt-bundle-mac-x86_64-20140321/sdk/platform-tools:/opt/dev_hdew/browsermob-proxy/bin:/Users/dgempesaw/.jenv/bin:/Users/dgempesaw/perl5/bin:usr/local/bin:/opt/adt-bundle-mac-x86_64-20140321/sdk/tools:/opt/adt-bundle-mac-x86_64-20140321/sdk/platform-tools:/opt/dev_hdew/browsermob-proxy/bin:/Users/dgempesaw/perl5/bin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin:/opt/X11/bin:/usr/texbin:/usr/texbin:/usr/texbin")
