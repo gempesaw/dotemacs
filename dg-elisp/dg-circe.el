1a0de18c7065fd781a20d182ec7344d05b3fb66f
Daniel Gempesaw <gempesaw@gmail.com>
2014-12-15 11:04:56 -0500

    Move bitlbee settings into dg-circe.el

--- dg-elisp/dg-circe.el
+++ dg-elisp/dg-circe.el
@@ -1,11 +1,31 @@
-(defvar freenode-password nil
+(defvar circe-freenode-password nil
   "pass for my freenode acc, it's not in repo obv")
+
+(defvar circe-bitlbee-password nil
+  "pass for local bitlbee server, not in repo")
+
 (setq circe-network-options
       `(("Freenode"
          :nick "dgempesaw"
          :channels ("#emacs" "#selenium")
-         :nickserv-password ,freenode-password
+         :nickserv-password ,circe-freenode-password
          )))
+
+(defun bitlbee ()
+  (interactive)
+  (save-window-excursion
+    (let ((circe-network-options '(("Bitlbee" :nowait-on-connect nil))))
+      (circe "Bitlbee")
+      (set-buffer "localhost:6667")
+      (with-circe-server-buffer
+        (circe-server-send
+         (format "PRIVMSG &bitlbee :identify %s" circe-bitlbee-password)
+         t))
+      (run-at-time "10 sec" nil (lambda ()
+                                  (set-buffer "localhost:6667")
+                                  (with-circe-server-buffer
+                                    (circe-server-send "PRIVMSG &bitlbee blist" t))
+                                  (bury-buffer "localhost:6667"))))))
 
 (setq circe-reduce-lurker-spam t)
 
@@ -13,7 +33,7 @@
 (defun my-circe-message-option-bot (nick &rest ignored)
   (when (member nick my-circe-bot-list)
     '((text-properties . (face circe-fool-face
-                          lui-do-not-track t)))))
+                               lui-do-not-track t)))))
 (add-hook 'circe-message-option-functions 'my-circe-message-option-bot)
 
 (provide 'dg-circe)
