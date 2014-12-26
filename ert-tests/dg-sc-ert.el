97d663f92786036004b678f61b510b9605b5d6eb
Daniel Gempesaw <gempesaw@gmail.com>
2014-12-26 02:52:16 -0500

    Remove obsolete tests

--- ert-tests/dg-sc-ert.el
+++ ert-tests/dg-sc-ert.el
@@ -1,14 +1,4 @@
 (require 'ert)
 (require 'dg-sc)
 
-(ert-deftest decide-what-to-restart ()
-  (should (sc--box-in-restart-group-p "scdwschedulemaster2f" "all"))
-  (should (not (sc--box-in-restart-group-p "scdwschedulemaster2f" "half")))
-  (should (not (sc--box-in-restart-group-p "scdwschedulemaster2f" "pubs")))
-  (should (sc--box-in-restart-group-p "scdwwebauth2f" "all"))
-  (should (sc--box-in-restart-group-p "scdwwebauth2f" "half"))
-  (should (not (sc--box-in-restart-group-p "scdwwebauth2f" "pubs")))
-  (should (not (sc--box-in-restart-group-p "scdwwebauth2f" "pub")))
-  (should (sc--box-in-restart-group-p "scdwwebpub2f" "pub")))
-
 (provide 'dg-sc-ert)
