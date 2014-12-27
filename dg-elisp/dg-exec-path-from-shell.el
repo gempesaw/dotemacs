334c02f90e9607b880a7ef22f20fd85fbe98be69
Daniel Gempesaw <gempesaw@gmail.com>
2014-12-26 22:14:09 -0500

    Store a reset version of exec-path in case we need it again

--- dg-elisp/dg-exec-path-from-shell.el
+++ dg-elisp/dg-exec-path-from-shell.el
@@ -4,4 +4,20 @@
 (when (getenv "PERL5LIB")
   (exec-path-from-shell-copy-env "PERL5LIB"))
 
+;;; reset the path if necessary
+;; (setq exec-path '("/usr/local/bin"
+;;                   "/usr/local/opt/android-sdk/tools"
+;;                   "/usr/local/opt/android-sdk/platform-tools"
+;;                   "/opt/dev_hdew/browsermob-proxy/bin"
+;;                   "/Users/dgempesaw/.jenv/bin"
+;;                   "/Users/dgempesaw/perl5/bin"
+;;                   "/usr/bin"
+;;                   "/bin"
+;;                   "/usr/sbin"
+;;                   "/sbin"
+;;                   "/usr/local/bin"
+;;                   "/opt/X11/bin"
+;;                   "/usr/texbin"
+;;                   "/Users/dgempesaw/.rvm/bin"))
+
 (provide 'dg-exec-path-from-shell)
