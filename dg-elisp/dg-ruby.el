287be196d91aaecdbe1100c4c1d2c82b7576a6bc
Daniel Gempesaw <gempesaw@gmail.com>
2014-12-27 21:17:52 -0500

    Fix loading order for ruby setup

--- dg-elisp/dg-ruby.el
+++ dg-elisp/dg-ruby.el
@@ -1,15 +1,19 @@
 (push 'company-robe company-backends)
 (add-hook 'ruby-mode-hook 'robe-mode)
 (add-hook 'ruby-mode-hook 'company-mode)
+(add-hook 'ruby-mode-hook 'flycheck-mode)
+(add-hook 'ruby-mode-hook 'projectile-rails-on)
 
-(progn
-  (rvm-use-default)
+(eval-after-load 'projectile-rails
+  (progn
+    (rvm-use-default)
 
-  (setq company-idle-delay 0)
+    (setq company-idle-delay 0)
 
-  (defadvice inf-ruby-console-auto (before activate-rvm-for-robe activate)
-    (rvm-activate-corresponding-ruby))
+    (defadvice inf-ruby-console-auto (before activate-rvm-for-robe activate)
+      (rvm-activate-corresponding-ruby))
 
-  (global-set-key (kbd "C-<tab>") 'company-complete))
+    (global-set-key (kbd "C-<tab>") 'company-complete)
+    (key-chord-define projectile-rails-mode-map "jk" 'projectile-rails-command-map)))
 
 (provide 'dg-ruby)
