a4c33c3944bda1aad6e7e63877a1ad568e0889dc
Daniel Gempesaw <gempesaw@gmail.com>
2014-12-29 11:02:53 -0500

    Make diary suggestion more noticeable

--- dg-elisp/dg-diary.el
+++ dg-elisp/dg-diary.el
@@ -1,27 +1,31 @@
 (require 'calendar)
 
 (calendar-set-date-style 'iso)
-(global-set-key (kbd "C-s-d") (lambda () (interactive)
-                                (toggle-app-and-home
-                                 "Calendar"
-                                 (lambda ()
-                                   (window-configuration-to-register 6245)
-                                   (calendar)
-                                   (delete-other-windows)
-                                   (text-scale-adjust 3)
-                                   (execute-kbd-macro [?m ?. ?i ?d])
-                                   (text-scale-adjust 0)
-                                   (text-scale-adjust 2)
-                                   (recenter-top-bottom)))))
 
-(defun remind-me-daily (msg time)
+(defun toggle-diary-windows ()
+  (interactive)
+  (toggle-app-and-home
+   "Calendar"
+   (lambda ()
+     (window-configuration-to-register 6245)
+     (calendar)
+     (delete-other-windows)
+     (text-scale-adjust 3)
+     (execute-kbd-macro [?m ?. ?i ?d])
+     (text-scale-adjust 0)
+     (text-scale-adjust 2)
+     (recenter-top-bottom))))
+
+(global-set-key (kbd "C-s-d") 'toggle-diary-windows)
+
+(defun remind-me-daily (fn time)
   (when (and (boundp 'reminder)
              (timerp reminder))
     (cancel-timer reminder))
   (let ((daily (* 60 60 24)))
     (setq reminder
-          (run-at-time time daily 'message msg))))
+          (run-at-time time daily 'funcall fn))))
 
-(remind-me-daily "Update your diary now!" "4:30pm")
+(remind-me-daily 'toggle-diary-windows "4:30pm")
 
 (provide 'dg-diary)
