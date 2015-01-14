fae6adc5d2bcca5b59b6cfab12500fcd7cd75778
Daniel Gempesaw <gempesaw@gmail.com>
2015-01-14 11:46:16 -0800

    Add and customize coffee mode

--- dg-elisp/dg-ruby.el
+++ dg-elisp/dg-ruby.el
@@ -4,6 +4,15 @@
 (add-hook 'ruby-mode-hook 'flycheck-mode)
 (add-hook 'ruby-mode-hook 'projectile-rails-on)
 
+(add-hook 'coffee-mode-hook 'projectile-rails-on)
+
+(setq coffee-tab-width 2)
+
+(defun disable-electric-indent ()
+  (set (make-local-variable 'electric-indent-functions)
+       (list (lambda (arg) 'no-indent))))
+
+(add-hook 'coffee-mode-hook 'disable-electric-indent)
 (eval-after-load 'projectile-rails
   (progn
     (rvm-use-default)
