440203b04636b536fbc0176b79cfba97ff3277dc
Daniel Gempesaw <gempesaw@gmail.com>
2015-01-22 10:17:48 -0500

    Install and configure rvm.el

---
+++ elpa/perlbrew-20130127.324/perlbrew-autoloads.el
@@ -0,0 +1,15 @@
+;;; perlbrew-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil nil ("perlbrew.el") (21695 52301 138806 0))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; perlbrew-autoloads.el ends here
---
+++ elpa/perlbrew-20130127.324/perlbrew-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "perlbrew" "20130127.324" "A perlbrew wrapper for Emacs" 'nil :keywords '("emacs" "perl"))
