5902e4305168c80e0c03a570e60240f3ebd804bf
Daniel Gempesaw <gempesaw@gmail.com>
2015-02-15 17:35:22 -0500

    Show count for ido-vertical

--- dg-elisp/dg-ido-vertical-mode.el
+++ dg-elisp/dg-ido-vertical-mode.el
@@ -1,3 +1,5 @@
-(setq ido-vertical-define-keys 'C-n-C-p-up-and-down)
+(setq ido-use-faces t
+      ido-vertical-show-count t
+      ido-vertical-define-keys 'C-n-C-p-up-and-down)
 
 (provide 'dg-ido-vertical-mode)
