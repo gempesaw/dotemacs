60ac3aa4fea75addbb7da91ee5d2708d1397dd89
Daniel Gempesaw <gempesaw@gmail.com>
2015-03-29 12:34:40 -0400

    Use js2-mode for .es6 extensions

--- dg-elisp/dg-js2-mode.el
+++ dg-elisp/dg-js2-mode.el
@@ -1,3 +1,11 @@
-(setq js2-global-externs '("angular" "CodeMirror"))
+(setq js2-global-externs '(
+                           "angular"
+                           "CodeMirror"
+                           "describe"
+                           "it"
+                           "iit"
+                           ))
+
+(add-to-list 'auto-mode-alist '("\\.es6\\'" . js2-mode))
 
 (provide 'dg-js2-mode)
