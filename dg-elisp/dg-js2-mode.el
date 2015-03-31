94df239becbe47fac90d94f238cb7654a04fc7bd
Daniel Gempesaw <gempesaw@gmail.com>
2015-03-31 09:31:56 -0400

    Customize tern mode and js2-mode

--- dg-elisp/dg-js2-mode.el
+++ dg-elisp/dg-js2-mode.el
@@ -6,6 +6,13 @@
                            "iit"
                            ))
 
+(delete '("\\.js\\'" . javascript-generic-mode) auto-mode-alist)
+(delete '("\\.js\\'" . js-mode) auto-mode-alist)
 (add-to-list 'auto-mode-alist '("\\.es6\\'" . js2-mode))
+(add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
+
+(add-hook 'js2-mode-hook (lambda () (tern-mode t)))
+(eval-after-load 'tern
+  '(require 'company-tern))
 
 (provide 'dg-js2-mode)
