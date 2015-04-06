6b9947caaf7e6c1cb76e28016e015e129f43d64d
Daniel Gempesaw <gempesaw@gmail.com>
2015-04-06 09:22:27 -0400

    Add some flexiblity to safe buffer cleanup

--- dg-elisp/dg-defun.el
+++ dg-elisp/dg-defun.el
@@ -192,14 +192,22 @@ them, asking user for confirmation"
              (set-window-start w2 s1)
              (setq i (1+ i)))))))
 
+(setq cleanup-buffer-tabs t)
 (defun cleanup-buffer-safe ()
   "Perform a bunch of safe operations on the whitespace content of a buffer.
 Does not indent buffer, because it is used for a before-save-hook, and that
 might be bad."
   (interactive)
-  (untabify (point-min) (point-max))
+  (when cleanup-buffer-tabs
+    (untabify (point-min) (point-max)))
   (delete-trailing-whitespace)
   (set-buffer-file-coding-system 'utf-8))
+
+(defun cleanup-buffer-toggle-tabs ()
+  (interactive)
+  (message
+   (format "tabs cleanup: %s"
+           (setq cleanup-buffer-tabs (not cleanup-buffer-tabs)))))
 
 (defun cleanup-buffer ()
   "Perform a bunch of operations on the whitespace content of a buffer.
