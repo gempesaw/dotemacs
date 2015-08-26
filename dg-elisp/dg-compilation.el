40318f54581aa8b5a8197aad669466875bfbb85d
Daniel Gempesaw <gempesaw@gmail.com>
2015-08-25 22:30:51 -0400

    Use smarter compilation colorization

--- dg-elisp/dg-compilation.el
+++ dg-elisp/dg-compilation.el
@@ -1,8 +1,10 @@
 (require 'ansi-color)
+
 (defun colorize-compilation-buffer ()
-  (toggle-read-only)
-  (ansi-color-apply-on-region (point-min) (point-max))
-  (toggle-read-only))
+  (let ((inhibit-read-only t))
+    (when (eq major-mode 'compilation-mode)
+      (ansi-color-apply-on-region compilation-filter-start (point-max)))))
+
 (add-hook 'compilation-filter-hook 'colorize-compilation-buffer)
 
 (provide 'dg-compilation)
