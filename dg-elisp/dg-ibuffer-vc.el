f5fd9d1bbf09b179b2239c2c3bd6c8c3b2018884
Daniel Gempesaw <gempesaw@gmail.com>
2015-08-25 22:32:47 -0400

    Stop using ibuffer-vc

--- dg-elisp/dg-ibuffer-vc.el
+++ dg-elisp/dg-ibuffer-vc.el
@@ -1,18 +1,11 @@
-(add-hook 'ibuffer-hook
-    (lambda ()
-      (ibuffer-vc-set-filter-groups-by-vc-root)
-      (unless (eq ibuffer-sorting-mode 'alphabetic)
-        (ibuffer-do-sort-by-alphabetic))))
-
+(setq ibuffer-hook nil)
 (setq ibuffer-formats
-      '((mark modified read-only vc-status-mini " "
+      '((mark modified read-only " "
               (name 18 18 :left :elide)
               " "
               (size 9 -1 :right)
               " "
               (mode 16 16 :left :elide)
-              " "
-              (vc-status 16 16 :left)
               " "
               filename-and-process)))
 
