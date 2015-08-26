08ed88f0bba4bc9e4384b5a378be53342a50c175
Daniel Gempesaw <gempesaw@gmail.com>
2015-08-25 22:41:24 -0400

    Update grunt version

--- elpa/grunt-20150129.1309/grunt-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "grunt" "20150129.1309" "Some glue to stick Emacs and Gruntfiles together" '((dash "2.6.0")) :url "https://github.com/gempesaw/grunt.el" :keywords '("convenience" "grunt"))
---
+++ elpa/grunt-20150820.36/grunt-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "grunt" "20150820.36" "Some glue to stick Emacs and Gruntfiles together" '((dash "2.9.0") (ansi-color "3.4.2")) :url "https://github.com/gempesaw/grunt.el" :keywords '("convenience" "grunt"))
