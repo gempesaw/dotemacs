809a181daf86f832f1292fb6d18144e607a9aa6f
Daniel Gempesaw <gempesaw@gmail.com>
2017-01-16 18:34:17 -0500

    Set dired subtree faces for light background

--- dg-elisp/dg-dired-subtree.el
+++ dg-elisp/dg-dired-subtree.el
@@ -1,14 +1,23 @@
+(require 'dired-subtree)
+
 (define-key dired-mode-map (kbd "i")
   (lambda ()
     (interactive)
     (dired-subtree-insert)
     (revert-buffer)))
 
-
 (define-key dired-mode-map (kbd "k")
   (lambda ()
     (interactive)
     (dired-subtree-remove)
     (revert-buffer)))
+
+(progn
+  (set-face-attribute 'dired-subtree-depth-1-face nil :background "#DAD1CF")
+  (set-face-attribute 'dired-subtree-depth-2-face nil :background "#DAD1CF")
+  (set-face-attribute 'dired-subtree-depth-3-face nil :background "#DAD1CF")
+  (set-face-attribute 'dired-subtree-depth-4-face nil :background "#DAD1CF")
+  (set-face-attribute 'dired-subtree-depth-5-face nil :background "#DAD1CF")
+  (set-face-attribute 'dired-subtree-depth-6-face nil :background "#DAD1CF"))
 
 (provide 'dg-dired-subtree)
