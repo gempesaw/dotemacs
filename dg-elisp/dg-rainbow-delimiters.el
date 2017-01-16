984a991ed48be7ae153e12d321a3c4114d155c60
Daniel Gempesaw <gempesaw@gmail.com>
2017-01-16 18:34:04 -0500

    Set rainbow delimiter color for light background

--- dg-elisp/dg-rainbow-delimiters.el
+++ dg-elisp/dg-rainbow-delimiters.el
@@ -2,11 +2,12 @@
 ;;; saturate paren colors
 (require 'cl-lib)
 (require 'color)
+
 (cl-loop
  for index from 1 to rainbow-delimiters-max-face-count
  do
  (let ((face (intern (format "rainbow-delimiters-depth-%d-face" index))))
-   (cl-callf color-saturate-name (face-foreground face) 30)))
+   (cl-callf color-saturate-name (face-foreground face) 10)))
 
 ;;; unmatched parens stick out
 (set-face-attribute 'rainbow-delimiters-unmatched-face nil
@@ -19,6 +20,5 @@
                     :weight 'bold)
 
 (add-hook 'prog-mode-hook 'rainbow-delimiters-mode)
-(global-rainbow-delimiters-mode t)
 
 (provide 'dg-rainbow-delimiters)
