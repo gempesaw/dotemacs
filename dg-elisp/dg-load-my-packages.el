badb64edebfbf4d55b34fda874f3ba9b4fb03782
Daniel Gempesaw <gempesaw@gmail.com>
2017-01-24 10:06:08 -0500

    Use dired+

--- dg-elisp/dg-load-my-packages.el
+++ dg-elisp/dg-load-my-packages.el
@@ -24,7 +24,7 @@
                     circe
                     dash
                     diminish
-                    dired-details
+                    dired+
                     dired-filter
                     dired-hacks-utils
                     flx-ido
