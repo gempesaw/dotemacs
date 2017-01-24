24be266cb3e8cc967677eff7a1e62de27ad93895
Daniel Gempesaw <gempesaw@gmail.com>
2017-01-24 10:07:19 -0500

    Add dired convenience functions

--- dg-elisp/dg-dired.el
+++ dg-elisp/dg-dired.el
@@ -1,10 +1,13 @@
 (require 'dired)
+(require 'dired+)
 (require 'dired-x)
 (require 'dired-aux)
 
 (eval-after-load "dired-aux"
    '(add-to-list 'dired-compress-file-suffixes
                  '("\\.zip\\'" ".zip" "unzip")))
+
+(define-key dired-mode-map "\M-g" nil)
 
 ;; Make dired less verbose
 (eval-after-load "dired-details"
@@ -36,6 +39,39 @@
       (setq path (concat path "/index.html")))
     (browse-url path)))
 
+(defun dg-tabulate-gatling-results ()
+  (interactive)
+  (let* ((gatling-result-folder (dired-copy-filename-as-kill 0))
+         (results (mapcar (lambda (it) (s-replace "\n" "" it))
+                          (s-split " "
+                                   (shell-command-to-string
+                                    (format "echo 'console.log(stats.stats.meanNumberOfRequestsPerSecond.total, stats.stats.percentiles3.total, stats.stats.meanResponseTime.total)' | cat %s/js/stats.js - | node"
+                                            gatling-result-folder)))))
+         (mean-rps (car results))
+         (95th-per (cadr results))
+         (mean-response (caddr results))
+         (url (format "http://10.10.2.233:8001/provider-server/build/reports/gatling/%s/" gatling-result-folder)))
+    (with-current-buffer "results"
+      (end-of-buffer)
+      (newline)
+      (insert (format  "| %s | %s | %s | [link|%s] |" mean-rps mean-response 95th-per url)))
+    (next-line)))
+
+(defun dg-upload-to-s3 ()
+  (interactive)
+  (let* ((base-path "s.qa.origin.sharecare.com/honeydew")
+         (folder-name (thing-at-point 'symbol t))
+         (args (format "put --recursive %s/ s3://%s/%s/"
+                       folder-name base-path folder-name))
+         (url (format "http://%s/%s" base-path folder-name)))
+    (apply 'start-process (append '("s3-upload" "*test*" "s3cmd") (s-split " " args)))
+    url))
+
+
 (define-key dired-mode-map "b" 'dg-dired-browse-file-at-point)
 
 (provide 'dg-dired)
+
+(progn
+  (let ((list '("a" "b" "c")))
+    (caddr list)))
