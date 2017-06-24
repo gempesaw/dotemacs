3e0ec0c93f72371f052541278013c48e081161b0
Daniel Gempesaw <gempesaw@gmail.com>
2017-06-24 10:04:58 -0400

    Update s-

--- elpa/s-20160928.636/s-autoloads.el
+++ elpa/s-20160928.636/s-autoloads.el
@@ -1,9 +1,9 @@
 ;;; s-autoloads.el --- automatically extracted autoloads
 ;;
 ;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
-;;;### (autoloads nil nil ("s.el") (21270 1292 216623 0))
+;;;### (autoloads nil nil ("s.el") t)
 
 ;;;***
 
