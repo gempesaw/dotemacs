ef69b2e83a1c431abc6c92c13fd499e6c76a3140
Daniel Gempesaw <gempesaw@gmail.com>
2018-06-19 16:18:25 -0400

    Delete unused packages

--- elpa/calfw-20150227.326/calfw-pkg.el
+++
@@ -1,5 +0,0 @@
-(define-package "calfw" "20150227.326" "Calendar view framework on Emacs" 'nil :url "https://github.com/kiwanami/emacs-calfw" :keywords
-  '("calendar"))
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
--- elpa/litable-20140306.236/litable-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "litable" "20140306.236" "dynamic evaluation replacement with emacs" '((dash "2.6.0")) :keywords ("lisp"))
