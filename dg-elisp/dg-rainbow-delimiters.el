c91930dbad942e8452cee75d13417123e548b751
Daniel Gempesaw <gempesaw@gmail.com>
2019-05-27 22:44:59 -0400

    Update rainbow mode and delimiters

--- dg-elisp/dg-rainbow-delimiters.el
+++ dg-elisp/dg-rainbow-delimiters.el
@@ -1,24 +1,28 @@
 ;;; customizations from http://yoo2080.wordpress.com/2013/09/08/living-with-rainbow-delimiters-mode/
 ;;; saturate paren colors
-;; (require 'cl-lib)
-;; (require 'color)
 
-;; (cl-loop
-;;  for index from 1 to rainbow-delimiters-max-face-count
-;;  do
-;;  (let ((face (intern (format "rainbow-delimiters-depth-%d-face" index))))
-;;    (cl-callf color-saturate-name (face-foreground face) 25)))
+(eval-after-load 'rainbow-delimiters
+  (progn
 
-;;; unmatched parens stick out
-(set-face-attribute 'rainbow-delimiters-unmatched-face nil
-                    :foreground 'unspecified
-                    :inherit 'error
-                    :strike-through t)
+    (require 'cl-lib)
+    (require 'color)
 
-;;; bold outermost parens
-(set-face-attribute 'rainbow-delimiters-depth-1-face nil
-                    :weight 'bold)
+    ;; (cl-loop
+    ;;  for index from 1 to rainbow-delimiters-max-face-count
+    ;;  do
+    ;;  (let ((face (intern (format "rainbow-delimiters-depth-%d-face" index))))
+    ;;    (cl-callf color-saturate-name (face-foreground face) 100)))
 
-(add-hook 'prog-mode-hook 'rainbow-delimiters-mode)
+    ;;; unmatched parens stick out
+    (set-face-attribute 'rainbow-delimiters-unmatched-face nil
+                        :foreground 'unspecified
+                        :inherit 'error
+                        :strike-through t)
+
+    ;;; bold outermost parens
+    (set-face-attribute 'rainbow-delimiters-depth-1-face nil
+                        :weight 'bold)
+
+    (add-hook 'prog-mode-hook 'rainbow-delimiters-mode)))
 
 (provide 'dg-rainbow-delimiters)
