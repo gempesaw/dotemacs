09b23c3627f92dd03a5ff1d487e6f053b5ef57c3
Daniel Gempesaw <dgempesaw@pagerduty.com>
2020-04-23 17:05:21 -0400

    Search in ag's default hidden folders

--- dg-elisp/dg-ag.el
+++ dg-elisp/dg-ag.el
@@ -1,4 +1,5 @@
-(setq ag-arguments '("--smart-case" "--column" "--stats" "--")
+;;; echo '/.git/' >> ~/.agignore
+(setq ag-arguments '("--smart-case" "--column" "--stats" "--hidden")
       ag-reuse-buffers t)
 
 (provide 'dg-ag)
