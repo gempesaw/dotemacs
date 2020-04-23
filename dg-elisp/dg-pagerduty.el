ac7d12301afcfd32c5b220c3f43cd3218cb0cda7
Daniel Gempesaw <dgempesaw@pagerduty.com>
2020-04-23 17:08:06 -0400

    pd: Create shortcut for running tests

---
+++ dg-elisp/dg-pagerduty.el
@@ -0,0 +1,11 @@
+(defun dg-pd-run-web-test ()
+  (interactive)
+  (save-excursion
+    (let
+        ((regex (progn
+                  (end-of-line)
+                  (search-backward "should '")
+                  (car (reverse (s-match "should .\\(.*\\). do" (current-line-contents))))
+                  ))
+         (file (file-relative-name buffer-file-name (projectile-project-root))))
+      (kill-new (format "bundle exec spring testunit %s -n \"/%s/\"" file regex)))))
