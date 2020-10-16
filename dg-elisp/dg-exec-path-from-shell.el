da804cfd3ef577e98444d7c26b9883b5dea485ee
Daniel Gempesaw <dgempesaw@pagerduty.com>
2020-10-16 11:49:40 -0400

    Add elixir-ls release dir to path
    so elixir lsp can work!

--- dg-elisp/dg-exec-path-from-shell.el
+++ dg-elisp/dg-exec-path-from-shell.el
@@ -1,7 +1,7 @@
 ;; (when (fboundp 'nvm-use) (nvm-use "v8.4.0"))
 
-(setenv "PATH" (concat (getenv "PATH") ":/opt/elixir-ls/release"))
-(setq exec-path (append exec-path '("/opt/elixir-ls/release")))
+(setenv "PATH" (concat (getenv "PATH") ":/Users/dgempesaw/opt/elixir-ls/release"))
+(setq exec-path (append exec-path '("/Users/dgempesaw/opt/elixir-ls/release")))
 
 
 (when (file-exists-p "~/.nvm")
