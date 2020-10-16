435a574e7064913e71240a970ccb09e520a1df9e
Daniel Gempesaw <dgempesaw@pagerduty.com>
2020-10-16 11:49:57 -0400

    Link to repo's homepage

--- dg-elisp/dg-git-link.el
+++ dg-elisp/dg-git-link.el
@@ -1,6 +1,7 @@
 (eval-after-load 'git-link
   '(progn
      (setq git-link-open-in-browser t)
-     (global-set-key (kbd "C-c l") 'git-link)))
+     (global-set-key (kbd "C-c l") 'git-link)
+     (global-set-key (kbd "C-c C-l") 'git-link-homepage)))
 
 (provide 'dg-git-link)
