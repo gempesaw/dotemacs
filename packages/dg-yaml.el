4236ce6712699532ed3accce7b9a0f9c31eb76fb
Daniel Gempesaw <gempesaw@gmail.com>
2023-01-04 21:32:32 -0500

    Add yaml-mode

--- packages/dg-yaml.el
+++ packages/dg-yaml.el
@@ -1,5 +1,11 @@
-(use-package highlight-indent-guides
+(use-package yaml-mode
   :requires (lsp)
+  :ensure t
   :config
-  (add-hook 'yaml-mode-hook 'lsp-deferred)
+  (add-hook 'yaml-mode-hook 'lsp-deferred))
+
+(use-package highlight-indent-guides
+  :ensure t
+  :requires (lsp yaml-mode)
+  :config
   (add-hook 'yaml-mode-hook 'highlight-indent-guides-mode))
