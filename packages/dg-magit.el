0b39e1aa81ef3da8a7e70501e0c6ceaa4aa97fea
Daniel Gempesaw <gempesaw@gmail.com>
2023-12-29 22:38:17 -0500

    Check in some random changes

--- packages/dg-magit.el
+++ packages/dg-magit.el
@@ -1,10 +1,10 @@
 (use-package magit
   :ensure t
   :bind (("C-x g" . magit-status)
-	 ("C-c g" . magit-blame))
+         ("C-c g" . magit-blame))
   :chords (("xg" . magit-status))
-  :config 
-  (setq magit-git-executable "/usr/local/bin/git")
+  :config
+  (setq magit-git-executable "/opt/homebrew/bin/git")
   ;; (setq magit-completing-read-function 'magit-completing-read)
 
   ;; be like dired, use d for killing
@@ -30,8 +30,8 @@
     (interactive)
     (let ((magit-process (get-buffer-process "*magit-process*")))
       (when magit-process
-	(set-process-query-on-exit-flag magit-process nil)
-	(kill-process magit-process))))
+        (set-process-query-on-exit-flag magit-process nil)
+        (kill-process magit-process))))
 
   (let ((homebrew-emacsclient "/usr/local/Cellar/emacs/HEAD/bin/emacsclient"))
     (when (file-exists-p homebrew-emacsclient)
@@ -46,7 +46,7 @@
     (when (file-exists-p "/opt")
       (let ((buf " *github clone*")
             (url (read-from-minibuffer "Repository to clone: " (current-kill 0) )))
-	(async-shell-command (format "cd /opt && git clone %s" url) buf buf ))))
+        (async-shell-command (format "cd /opt && git clone %s" url) buf buf ))))
 
   (defun dg-magit-browse-remote ()
     (interactive)
@@ -59,4 +59,3 @@
       (call-interactively 'magit-clone)))
 
   (setq magit-clone-set-remote.pushDefault t))
-
