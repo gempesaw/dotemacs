e0135f664f68c4d97190befdfd8d0b2c7eb6b4e7
Daniel Gempesaw <gempesaw@gmail.com>
2025-07-07 23:02:18 -0400

    Disable combobulate; i can't remember using it

--- packages/dg-combobulate.el
+++ packages/dg-combobulate.el
@@ -1,12 +1,12 @@
-(use-package combobulate
-  :demand t
-  :hook ((python-ts-mode . combobulate-mode)
-         (js-ts-mode . combobulate-mode)
-         (css-ts-mode . combobulate-mode)
-         (yaml-ts-mode . combobulate-mode)
-         (typescript-ts-mode . combobulate-mode)
-         (tsx-ts-mode . combobulate-mode))
-  :load-path "combobulate"
-  :config
-  (define-key combobulate-key-map (kbd "M") 'self-insert-command)
-  (define-key combobulate-key-map (kbd "M-k") nil))
+;; (use-package combobulate
+;;   :demand t
+;;   :hook ((python-ts-mode . combobulate-mode)
+;;          (js-ts-mode . combobulate-mode)
+;;          (css-ts-mode . combobulate-mode)
+;;          (yaml-ts-mode . combobulate-mode)
+;;          (typescript-ts-mode . combobulate-mode)
+;;          (tsx-ts-mode . combobulate-mode))
+;;   :load-path "combobulate"
+;;   :config
+;;   (define-key combobulate-key-map (kbd "M") 'self-insert-command)
+;;   (define-key combobulate-key-map (kbd "M-k") nil))
