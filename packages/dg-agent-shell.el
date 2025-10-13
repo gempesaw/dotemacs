c6e477018acbee64f857e6723a59d9d08e35ee4e
Daniel Gempesaw <gempesaw@gmail.com>
2025-10-13 12:46:11 -0400

    Start using agent-shell

---
+++ packages/dg-agent-shell.el
@@ -0,0 +1,11 @@
+(use-package shell-maker
+  :ensure t
+  :demand t)
+
+(use-package acp
+  :vc (:url "https://github.com/xenodium/acp.el")
+  :ensure t)
+
+(use-package agent-shell
+  :vc (:url "https://github.com/xenodium/agent-shell")
+  :ensure t)
