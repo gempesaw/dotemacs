aadb291b0b7f8d41c108f18834d4d6c2bfb8b31f
Daniel Gempesaw <gempesaw@gmail.com>
2026-01-26 11:11:11 -0500

    Add fzf auto-detection and unbind C-c k/j in coterm char mode

--- packages/dg-coterm.el
+++ packages/dg-coterm.el
@@ -12,6 +12,32 @@
       (posframe-delete-frame b)
       (kill-buffer b)))
 
+  ;; Add fzf detection to coterm auto-char functions
+  (defun dg-coterm--auto-char-fzf ()
+    "Enter `coterm-char-mode' if fzf is running.
+Detects fzf by looking for its characteristic prompt pattern."
+    (when (and (eobp)
+               (save-excursion
+                 (forward-line -1)
+                 ;; Look for fzf's characteristic ">" prompt or info line
+                 (or (looking-at "^>")
+                     (looking-at "^  [0-9]+/[0-9]+")
+                     ;; Also check if process name contains fzf
+                     (when-let* ((proc (get-buffer-process (current-buffer)))
+                                 (cmd (process-command proc)))
+                       (seq-some (lambda (arg) (string-match-p "fzf" arg)) cmd)))))
+      (unless coterm-char-mode (coterm-char-mode 1))
+      (unless coterm-scroll-snap-mode (coterm-scroll-snap-mode 1))
+      t))
+
+  ;; Add our fzf detector to the beginning of the hook list
+  (add-hook 'coterm-auto-char-functions #'dg-coterm--auto-char-fzf)
+
+  ;; Allow C-c k and C-c j to pass through to Emacs in Char mode
+  (define-key coterm-char-mode-map (kbd "C-c k") nil)
+  (define-key coterm-char-mode-map (kbd "C-c j") nil)
+  (define-key coterm-char-mode-map (kbd "M-j") nil)
+
   :bind ((:map comint-mode-map ("C-;" . #'coterm-char-mode-cycle))
          (:map dired-mode-map
                ("V" . dg-shell-exec-at-point)))
