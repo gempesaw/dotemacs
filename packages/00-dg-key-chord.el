d28ada6af5151baf5d8f1ec090c0ca801993ad92
Daniel Gempesaw <gempesaw@gmail.com>
2026-02-27 10:36:21 -0500

    Disable visual-line-mode during avy-goto-char

--- packages/00-dg-key-chord.el
+++ packages/00-dg-key-chord.el
@@ -21,7 +21,10 @@
                                       (key-chord-mode t))))
 
   ;; movement, shells
-  (key-chord-define-global "fj" 'avy-goto-char)
+  (key-chord-define-global "fj" (lambda ()
+                                  (interactive)
+                                  (let ((visual-line-mode nil))
+                                    (call-interactively 'avy-goto-char))))
 
   ;; expanding region
   (key-chord-define-global "qk" 'er/expand-region)
