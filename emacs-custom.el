24fd8b609d655d7d4ef779b84541f1a5dcf40907
Daniel Gempesaw <gempesaw@gmail.com>
2026-09-02 16:02:24 -0400

    Drop jsonnet
    Takes the lsp-mode client registration, jsonnet-mode, apheleia's
    jsonnetfmt formatter, and the 12MB language server binary with it. The
    binary was the only reason the gitignore needed a section for
    unportable downloads, so that goes too.

--- emacs-custom.el
+++ emacs-custom.el
@@ -9,7 +9,7 @@
         dockerfile-mode dumb-jump duplicate-thing eat edit-server
         elisp-slime-nav elixir-mode epc exec-path-from-shell expand-region forge
         ghostel git-commit git-link git-timemachine go-mode gptel helpful
-        highlight-indent-guides js2-mode jsonnet-mode kubernetes-tramp loop
+        highlight-indent-guides js2-mode kubernetes-tramp loop
         lsp-docker lsp-pyright lsp-ui magit-delta magit-todos marginalia matisse
         mini-frame minions moody multiple-cursors nginx-mode noflet
         orderless org-jira paredit pet projectile
--- packages/dg-apheleia.el
+++ packages/dg-apheleia.el
@@ -6,14 +6,10 @@
   (apheleia-global-mode +1)
   (add-to-list 'apheleia-mode-alist '(python-mode . (ruff-isort ruff)))
   (add-to-list 'apheleia-mode-alist '(python-ts-mode . (ruff-isort ruff)))
-  (add-to-list 'apheleia-mode-alist '(jsonnet-mode . (jsonnetfmt)))
 
   (add-to-list 'apheleia-formatters '(ruff-isort "ruff" "check" "-n" "--select" "I" "--select" "F401" "--fix" "--fix-only" "--stdin-filename" filepath "-"))
   (add-to-list 'apheleia-formatters '(ruff . ("ruff" "format"
                                               "--verbose"
                                               (apheleia-formatters-fill-column "--line-length")
                                               "--stdin-filename" filepath
-                                              "-")))
-
-
-  (add-to-list 'apheleia-formatters '(jsonnetfmt "jsonnetfmt" "--in-place" filepath)))
+                                              "-"))))
